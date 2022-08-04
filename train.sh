377e0ed5d4dd62a9c5de29e3bf2871ad86610698
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-08-04 13:02:30 +0100

    Added GPU ID to train.sh

--- train.sh
+++ train.sh
@@ -5,6 +5,6 @@
 
 
 
-python code/models_train.py --dataset CUB2002011 --base_architecture resnet18 --batchsize 16 --num_workers 4
-python code/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --num_workers 4
-python code/models_train.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --num_workers 4
+python code/models_train.py --dataset CUB2002011 --base_architecture resnet18 --batchsize 16 --num_workers 4 --gpu_id 0
+python code/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --num_workers 4 --gpu_id 0
+python code/models_train.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --num_workers 4 --gpu_id 0
