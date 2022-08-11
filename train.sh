c7427443870a466a131a0c8e2c164f72d9e42d84
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-08-11 15:01:57 +0100

    Added train.sh with STANFORDCARS data.

--- train.sh
+++ train.sh
@@ -5,12 +5,12 @@
 
 
 # CUB2002011 "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
-python code/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --num_workers 3 --gpu_id 0
-python code/models_train.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 32 --num_workers 3 --gpu_id 0
-python code/models_train.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --num_workers 3 --gpu_id 0
-python code/models_train.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 32 --num_workers 3 --gpu_id 0
-python code/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 32 --num_workers 3 --gpu_id 0
-python code/models_train.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 32 --num_workers 3 --gpu_id 0
+# python code/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --num_workers 3 --gpu_id 0
+# python code/models_train.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 32 --num_workers 3 --gpu_id 0
+# python code/models_train.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --num_workers 3 --gpu_id 0
+# python code/models_train.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 32 --num_workers 3 --gpu_id 0
+# python code/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 32 --num_workers 3 --gpu_id 0
+# python code/models_train.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 32 --num_workers 3 --gpu_id 0
 
 # STANFORDCARS "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
 python code/models_train.py --dataset STANFORDCARS --base_architecture densenet121 --batchsize 32 --num_workers 3 --gpu_id 0
