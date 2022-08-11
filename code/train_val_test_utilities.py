36b38f2b1db4ef37b8e2ffbec08b94d8c7f20d1f
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-08-11 15:37:07 +0100

    Minor bugs with training in STANFORDCARS.

--- code/train_val_test_utilities.py
+++ code/train_val_test_utilities.py
@@ -93,7 +93,9 @@ def run_model(model, dataloader, mode, device, optimizer=None, class_specific=Tr
                 # Calculate cluster cost
                 # prototypes_of_correct_class = torch.t(model.module.prototype_class_identity[:,label]).cuda()
                 # prototypes_of_correct_class = torch.t(model.module.prototype_class_identity[:,label]).to(device)
-                print(labels.shape)
+                labels_ = torch.reshape(labels, (-1,))
+                print(labels_.shape)
+                print(labels_)
                 prototypes_of_correct_class = torch.t(model.prototype_class_identity[:,labels]).to(device)
                 inverted_distances, _ = torch.max((max_dist - min_distances) * prototypes_of_correct_class, dim=1)
                 cluster_cost = torch.mean(max_dist - inverted_distances)
