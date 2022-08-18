7fc24f0cc1203b8bf5756d5701a8fcf92ae0e397
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-08-18 15:59:24 +0100

    Added several parameters to prune_cub2002011.sh

--- prune_cub2002011.sh
+++ prune_cub2002011.sh
@@ -6,11 +6,14 @@
 
 
 # CUB2002011 "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
-python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-08-10_11-27-14/
-python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 16 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/
-python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/
-python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 32 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/
-python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 16 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/
-python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/
 
-echo "Finished."
+echo "Prunning | CUB2002011 | STARTED"
+
+python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-08-10_11-27-14/ --k 6 --prune_threshold 3
+python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 16 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/densenet161/2022-08-12_11-22-42/ --k 6 --prune_threshold 3
+python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet34/2022-08-16_07-33-07/ --k 6 --prune_threshold 3
+# python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 32 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/ --k 6 --prune_threshold 3
+# python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 16 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/ --k 6 --prune_threshold 3
+# python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --optimize_last_layer --num_workers 3 --gpu_id 0 --checkpoint cub2002011/ --k 6 --prune_threshold 3
+
+echo "Prunning | CUB2002011 | FINISHED"
