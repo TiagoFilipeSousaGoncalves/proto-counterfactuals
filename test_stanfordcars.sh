ae9836fdcd6969d59a881c3f10e16c6042b37d97
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-08-18 17:09:36 +0100

    Added new stuff test_stanfordcars.sh, train_stanfordcars.sh.

--- test_stanfordcars.sh
+++ test_stanfordcars.sh
@@ -11,7 +11,7 @@ echo "STANFORDCARS"
 python code/models_test.py --dataset STANFORDCARS --base_architecture densenet121 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint stanfordcars/densenet121/2022-08-11_16-16-36/
 # python code/models_test.py --dataset STANFORDCARS --base_architecture densenet161 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint
 python code/models_test.py --dataset STANFORDCARS --base_architecture resnet34 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint stanfordcars/resnet34/2022-08-14_13-44-35/
-python code/models_test.py --dataset STANFORDCARS --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint stanfordcars/resnet152/2022-08-16_21-51-41
+# python code/models_test.py --dataset STANFORDCARS --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint
 # python code/models_test.py --dataset STANFORDCARS --base_architecture vgg16 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint
 # python code/models_test.py --dataset STANFORDCARS --base_architecture vgg19 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint
 
