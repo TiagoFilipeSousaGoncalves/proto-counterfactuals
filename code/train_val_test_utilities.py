677f94ca4463f1440a23f9524dcfd586e0c512a7
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-09-05 09:24:41 +0100

    Added some debug prints.

--- code/train_val_test_utilities.py
+++ code/train_val_test_utilities.py
@@ -79,8 +79,8 @@ def run_model(model, dataloader, mode, device, optimizer=None, class_specific=Tr
             
             # We pass image by the model    
             logits, min_distances = model(images)
-            print(logits.type())
-            print(labels.type())
+            print(logits.type(), logits)
+            print(labels.type(), labels)
 
             # We first compute the CrossEntropy Loss
             cross_entropy = torch.nn.functional.cross_entropy(logits, labels)
