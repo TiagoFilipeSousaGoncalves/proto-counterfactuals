a9b7253fa9b3a63f5a2d7e133a1ff54960913f31
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-09-05 01:03:01 +0100

    Added proper test files.

--- test_cub2002011.sh
+++ test_cub2002011.sh
@@ -12,7 +12,7 @@ python code/models_test.py --dataset CUB2002011 --base_architecture densenet121 
 python code/models_test.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/densenet161/2022-08-12_11-22-42/
 python code/models_test.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet34/2022-08-16_07-33-07/
 python code/models_test.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet152/2022-08-19_18-07-45/
-# python code/models_test.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/
-# python code/models_test.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/
+python code/models_test.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/vgg16/2022-08-23_18-52-48/
+python code/models_test.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/vgg19/2022-08-26_08-12-03/
 
 echo "Finished."
--- test_stanfordcars.sh
+++ test_stanfordcars.sh
@@ -11,8 +11,8 @@ echo "STANFORDCARS"
 python code/models_test.py --dataset STANFORDCARS --base_architecture densenet121 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint stanfordcars/densenet121/2022-08-11_16-16-36/
 python code/models_test.py --dataset STANFORDCARS --base_architecture densenet161 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint stanfordcars/densenet161/2022-08-21_00-52-17/
 python code/models_test.py --dataset STANFORDCARS --base_architecture resnet34 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint stanfordcars/resnet34/2022-08-14_13-44-35/
-# python code/models_test.py --dataset STANFORDCARS --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint
-# python code/models_test.py --dataset STANFORDCARS --base_architecture vgg16 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint
-# python code/models_test.py --dataset STANFORDCARS --base_architecture vgg19 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint
+python code/models_test.py --dataset STANFORDCARS --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint stanfordcars/resnet152/2022-08-26_10-59-27/
+python code/models_test.py --dataset STANFORDCARS --base_architecture vgg16 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint stanfordcars/vgg16/2022-09-02_13-24-39/
+python code/models_test.py --dataset STANFORDCARS --base_architecture vgg19 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint stanfordcars/vgg19/2022-09-04_11-05-28/
 
 echo "Finished."
