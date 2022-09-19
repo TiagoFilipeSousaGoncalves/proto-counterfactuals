424019fab953ec99de0957b7c82ba5a5b9992aed
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-09-19 17:48:22 +0100

    Changed number of workers in global_analysis_cub2002011.sh

--- global_analysis_cub2002011.sh
+++ global_analysis_cub2002011.sh
@@ -11,7 +11,7 @@
 # CUB2002011 "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
 echo "CUB2002011"
 
-python code/models_global_analysis.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 64 --num_workers 2 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/
+python code/models_global_analysis.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 64 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/
 # python code/models_test.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/densenet161/2022-08-12_11-22-42/
 # python code/models_test.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet34/2022-08-16_07-33-07/
 # python code/models_test.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet152/2022-08-19_18-07-45/
