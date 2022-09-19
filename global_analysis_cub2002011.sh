cf3516cbd8ba7c87c5f2ebebcbdb03b250dbfc8d
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-09-19 19:00:38 +0100

    Added new partitions: global_analysis_cub2002011.sh, test_cub2002011.sh

--- global_analysis_cub2002011.sh
+++ global_analysis_cub2002011.sh
@@ -11,9 +11,9 @@
 # CUB2002011 "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
 echo "CUB2002011"
 
-python code/models_global_analysis.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 64 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/
-# python code/models_test.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/densenet161/2022-08-12_11-22-42/
-# python code/models_test.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet34/2022-08-16_07-33-07/
+python code/models_global_analysis.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/
+python code/models_global_analysis.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 16 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/densenet161/2022-09-15_16-14-45/
+python code/models_global_analysis.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 16 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/resnet34/2022-09-17_17-03-33/
 # python code/models_test.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet152/2022-08-19_18-07-45/
 # python code/models_test.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/vgg16/2022-08-23_18-52-48/
 # python code/models_test.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/vgg19/2022-08-26_08-12-03/
--- test_cub2002011.sh
+++ test_cub2002011.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH -p gtx1080ti_11GB                   # Partition        (check w/ $sinfo)
+#SBATCH -p fast_prototyping                   # Partition        (check w/ $sinfo)
 #SBATCH --job-name=test_cub2002011          # Job name
 #SBATCH -c 2                                # Number of cores
 #SBATCH -o slurm.%N.%j.out                  # STDOUT
@@ -11,9 +11,9 @@
 # CUB2002011 "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
 echo "CUB2002011"
 
-python code/models_test.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --num_workers 2 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/
-python code/models_test.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 32 --num_workers 2 --gpu_id 0 --checkpoint cub2002011/densenet161/2022-09-15_16-14-45/
-python code/models_test.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --num_workers 2 --gpu_id 0 --checkpoint cub2002011/resnet34/2022-09-17_17-03-33/
+python code/models_test.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --num_workers 2 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/
+python code/models_test.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 16 --num_workers 2 --gpu_id 0 --checkpoint cub2002011/densenet161/2022-09-15_16-14-45/
+python code/models_test.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 16 --num_workers 2 --gpu_id 0 --checkpoint cub2002011/resnet34/2022-09-17_17-03-33/
 # python code/models_test.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet152/
 # python code/models_test.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/vgg16/
 # python code/models_test.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/vgg19/
