fc916c9bc068dfee0bf482c588ebd30ba37204b3
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-09-19 17:20:50 +0100

    Modified code/models_global_analysis.py and added global_analysis_cub2002011.sh

--- code/models_global_analysis.py
+++ code/models_global_analysis.py
@@ -3,7 +3,6 @@
 
 # Imports
 import os
-import re
 import argparse
 import numpy as np
 
@@ -14,7 +13,7 @@ import torchvision
 
 # Project Imports
 from analysis_utilities import find_k_nearest_patches_to_prototypes, save_prototype_original_img_with_bbox
-from data_utilities import preprocess_input_function, CUB2002011Dataset, STANFORDCARSDataset
+from data_utilities import preprocess_input_function, CUB2002011Dataset, PH2Dataset, STANFORDCARSDataset
 from model_utilities import construct_PPNet
 
 
@@ -36,11 +35,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type=str, default="data", help="Directory of the data set.")
 
 # Data set
-parser.add_argument('--dataset', type=str, required=True, choices=["CUB2002011", "STANFORDCARS"], help="Data set: CUB2002011, STANFORDCARS.")
+parser.add_argument('--dataset', type=str, required=True, choices=["CUB2002011", "PH2", "STANFORDCARS"], help="Data set: CUB2002011, PH2, STANFORDCARS.")
 
 # Model
-# base_architecture = 'vgg19'
-parser.add_argument('--base_architecture', type=str, required=True, choices=["densenet121", "resnet18", "vgg19"], help='Base architecture: densenet121, resnet18, vgg19, ')
+parser.add_argument('--base_architecture', type=str, required=True, choices=["densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"], help='Base architecture: densenet121, resnet18, vgg19.')
 
 # Batch size
 parser.add_argument('--batchsize', type=int, default=4, help="Batch-size for training and validation")
@@ -51,7 +49,7 @@ parser.add_argument('--img_size', type=int, default=224, help="Size of the image
 
 # Prototype shape
 # prototype_shape = (2000, 128, 1, 1)
-parser.add_argument('--prototype_shape', type=tuple, default=(2000, 128, 1, 1), help="Prototype shape.")
+# parser.add_argument('--prototype_shape', type=tuple, default=(2000, 128, 1, 1), help="Prototype shape.")
 
 # Prototype Activation Function
 # prototype_activation_function = 'log'
@@ -83,6 +81,7 @@ parser.add_argument("--k_nearest_patches", type=int, default=5, help="The number
 
 # Parse the arguments
 args = parser.parse_args()
+
 
 
 # Get checkpoint (that contains the weights)
@@ -133,39 +132,41 @@ K = args.k_nearest_patches
 
 
 
-
 # Load the data
 # Note: Must use unaugmented (original) dataset
 # train_dir = train_push_dir
 
-# Train Transforms (Do not normalize)
+# Train Transforms (without normalization)
 train_transforms = torchvision.transforms.Compose([
     torchvision.transforms.Resize((IMG_SIZE, IMG_SIZE)),
     torchvision.transforms.ToTensor(),
 ])
 
-# Test Transforms (Do not normalize)
+
+# Test Transforms (without normalization)
 test_transforms = torchvision.transforms.Compose([
     torchvision.transforms.Resize((IMG_SIZE, IMG_SIZE)),
     torchvision.transforms.ToTensor(),
 ])
 
 
+
 # Dataset
 # CUB2002011
 if DATASET == "CUB2002011":
-
-    # Train
+    # Train Dataset
     train_set = CUB2002011Dataset(
         data_path=os.path.join(DATA_DIR, "cub2002011", "processed_data", "train", "cropped"),
         classes_txt=os.path.join(DATA_DIR, "cub2002011", "source_data", "classes.txt"),
+        augmented=True,
         transform=train_transforms
     )
 
-    # Test
+    # Test Dataset
     test_set = CUB2002011Dataset(
         data_path=os.path.join(DATA_DIR, "cub2002011", "processed_data", "test", "cropped"),
         classes_txt=os.path.join(DATA_DIR, "cub2002011", "source_data", "classes.txt"),
+        augmented=False,
         transform=test_transforms
     )
 
@@ -173,28 +174,68 @@ if DATASET == "CUB2002011":
     NUM_CLASSES = len(train_set.labels_dict)
 
 
-
-# STANFORDCARS
-elif DATASET == "STANFORDCARS":
-
-    # Train
-    train_set = STANFORDCARSDataset(
+# PH2
+elif DATASET == "PH2":
+    # Train Dataset
+    train_set = PH2Dataset(
         data_path=DATA_DIR,
-        cars_subset="cars_train",
+        subset="train",
         cropped=True,
         transform=train_transforms
     )
 
-    # Test
+    # Test Dataset
+    test_set = PH2Dataset(
+        data_path=DATA_DIR,
+        subset="test",
+        cropped=True,
+        transform=test_transforms
+    )
+
+    # Number of Classes
+    NUM_CLASSES = len(train_set.diagnosis_dict)
+
+
+# STANFORDCARS
+elif DATASET == "STANFORDCARS":
+    # Train Dataset
+    train_set = STANFORDCARSDataset(
+        data_path=DATA_DIR,
+        cars_subset="cars_train",
+        augmented=True,
+        cropped=True,
+        transform=train_transforms
+    )
+
+    # Test Dataset
     test_set = STANFORDCARSDataset(
         data_path=DATA_DIR,
         cars_subset="cars_test",
+        augmented=False,
         cropped=True,
         transform=test_transforms
     )
 
     # Number of classes
     NUM_CLASSES = len(train_set.class_names)
+
+
+
+# Define prototype shape according to original paper
+# The number of prototypes can be chosen with prior domain knowledge or hyperparameter search: we used 10 prototypes per class
+NUM_PROTOTYPES_CLASS = int(NUM_CLASSES * 10)
+
+# For VGG-16, VGG-19, DenseNet-121, DenseNet-161, we used 128 as the number of channels in a prototype
+if BASE_ARCHITECTURE.lower() in ("densenet121", "densenet161", "vgg16", "vgg19"):
+    PROTOTYPE_SHAPE = (NUM_PROTOTYPES_CLASS, 128, 1, 1)
+
+# For ResNet-34, we used 256 as the number of channels in a prototype;
+elif BASE_ARCHITECTURE.lower() in ("resnet34"):
+    PROTOTYPE_SHAPE = (NUM_PROTOTYPES_CLASS, 256, 1, 1)
+
+# For ResNet-152, we used 512 as the number of channels in a prototype
+elif BASE_ARCHITECTURE.lower() in ("resnet152"):
+    PROTOTYPE_SHAPE = (NUM_PROTOTYPES_CLASS, 512, 1, 1)
 
 
 
@@ -206,8 +247,11 @@ elif DATASET == "STANFORDCARS":
 # epoch_number_str = re.search(r'\d+', load_model_name).group(0)
 # start_epoch_number = int(epoch_number_str)
 
+
+
 # Results and Weights
 weights_dir = os.path.join(results_dir, "weights")
+
 
 
 # Construct the Model
@@ -225,9 +269,9 @@ ppnet_model = construct_PPNet(
 class_specific = True
 
 
-
 # Put model into DEVICE (CPU or GPU)
 ppnet_model = ppnet_model.to(DEVICE)
+
 
 # Load model weights
 model_path = os.path.join(weights_dir, f"{BASE_ARCHITECTURE.lower()}_{DATASET.lower()}_best.pt")
@@ -237,13 +281,12 @@ print("Model weights loaded with success.")
 
 
 
-
 # DataLoaders
 # Train
-train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS, pin_memory=False)
+train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=False, num_workers=WORKERS, pin_memory=False)
 
 # Test
-test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS, pin_memory=False)
+test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=BATCH_SIZE, shuffle=False, num_workers=WORKERS, pin_memory=False)
 
 
 
@@ -254,7 +297,7 @@ if not os.path.isdir(root_dir_for_saving_train_images):
 
 
 # Root directory for saving test images
-root_dir_for_saving_test_images = os.path.join(weights_dir, f'K{K}_nearest_test')
+root_dir_for_saving_test_images = os.path.join(weights_dir, f'K_{K}_nearest_test')
 if not os.path.isdir(root_dir_for_saving_test_images):
     os.makedirs(root_dir_for_saving_test_images)
 
@@ -302,12 +345,13 @@ for j in range(ppnet_model.num_prototypes):
 
 
 
-# Find K-Nearest Patches (Train)
+# Find K-Nearest Patches (Train) 
+# (in original code K -> K+1)
 find_k_nearest_patches_to_prototypes(
         dataloader=train_loader, # pytorch dataloader (must be unnormalized in [0,1])
         prototype_network_parallel=ppnet_model, # pytorch network with prototype_vectors
         device=DEVICE,
-        k=K+1,
+        k=K,
         preprocess_input_function=preprocess_input_function, # normalize if needed
         full_save=True,
         root_dir_for_saving_images=root_dir_for_saving_train_images,
@@ -324,3 +368,5 @@ find_k_nearest_patches_to_prototypes(
         full_save=True,
         root_dir_for_saving_images=root_dir_for_saving_test_images,
 )
+
+print("Finished.")
