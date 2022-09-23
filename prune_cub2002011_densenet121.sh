03dc4fc6d57eec61e1cc09be32a36cdaa62eae40
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-09-23 12:58:38 +0100

    Added pruning scripts to CUB2002011 database.

--- prune_cub2002011_densenet121.sh
+++ prune_cub2002011_densenet121.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH -p gtx1080ti_11GB                 # Partition        (check w/ $sinfo)
-#SBATCH --job-name=tr_stan_dn161          # Job name
+#SBATCH --job-name=pr_cub_dn161          # Job name
 #SBATCH -o slurm.%N.%j.out                # STDOUT
 #SBATCH -e slurm.%N.%j.err                # STDERR
 #SBATCH --reservation=tgoncalv_1          # Reservation Name
@@ -12,6 +12,6 @@
 
 echo "Prunning | CUB2002011:DenseNet121 | STARTED"
 
-python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --optimize_last_layer --num_workers o --gpu_id 0 --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/ --k 6 --prune_threshold 3
+python code/models_prototype_pruning.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --optimize_last_layer --num_workers 0 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/ --k 6 --prune_threshold 3
 
 echo "Prunning | CUB2002011 | FINISHED"
