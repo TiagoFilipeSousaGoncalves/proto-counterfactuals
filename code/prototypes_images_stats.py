8764981fe607e4be6526c53be0c976d631644153
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-09-26 14:13:49 +0100

    modified:   code/prototypes_images_stats.py

--- code/prototypes_images_stats.py
+++ code/prototypes_images_stats.py
@@ -27,4 +27,9 @@ CHECKPOINT = args.checkpoint
 
 # Open the .CSV file
 proto_stats_df = pd.read_csv(filepath_or_buffer=os.path.join(CHECKPOINT, "analysis", "local", "analysis.csv"), sep=",", header=0)
-print(proto_stats_df.head())
+# print(proto_stats_df.head())
+
+
+# Get rows where ground-truth is equal to the predicted label
+proto_stats_pr_df = proto_stats_df.copy()[proto_stats_df["Ground-Truth Label"]==proto_stats_df["Predicted Label"]]
+print(proto_stats_pr_df.head())
