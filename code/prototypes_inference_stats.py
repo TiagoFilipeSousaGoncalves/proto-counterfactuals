e0ca23ea986dffa8dd7fbc97e628ba2ca9fd3276
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-10-06 12:06:26 +0100

    Added code/prototypes_inference_stats.py and proto_inf_stats_cub2002011.sh

---
+++ code/prototypes_inference_stats.py
@@ -0,0 +1,89 @@
+# Imports
+import os
+import argparse
+import pandas as pd
+
+
+
+# Command Line Interface
+# Create the parser
+parser = argparse.ArgumentParser()
+
+# Add the arguments
+# Data directory
+parser.add_argument('--checkpoint', type=str, default="data", help="Path to the model checkpoint.")
+
+
+
+# Parse the arguments
+args = parser.parse_args()
+
+
+
+# Get the path of the CSV that contains the analysis
+CHECKPOINT = args.checkpoint
+
+
+
+# Open the .CSV file
+proto_stats_df = pd.read_csv(filepath_or_buffer=os.path.join("results", CHECKPOINT, "analysis", "counterfac-inf", "inference_results.csv"), sep=",", header=0)
+# print(proto_stats_df.head())
+
+
+# Get rows where ground-truth is equal to the predicted label
+proto_stats_pr_df = proto_stats_df.copy()[["Image Index", "Ground-Truth Label", "Predicted Label", "Predicted Scores", "Counterfactuals"]][proto_stats_df["Ground-Truth Label"]==proto_stats_df["Predicted Label"]]
+print(proto_stats_pr_df.head())
+
+
+"""
+# Create a anoter column to count the number of prototypes (out of the most activated) that are related to the class identity
+proto_stats_pr_df["Out-of-TopK Identity Activated Prototypes"] = 0
+# print(proto_stats_pr_df.head())
+
+
+# Reset index so that indices match the number of rows
+proto_stats_pr_df = proto_stats_pr_df.reset_index()
+
+# Iterate through rows
+for index, row in proto_stats_pr_df.iterrows():
+
+    # Get label
+    label = row["Ground-Truth Label"]
+
+    # Get the cls identity of top-k most activated prototypes
+    top_k_proto = row["Top-10 Activated Prototypes"]
+
+    # Apply a processing to this string
+    top_k_proto = top_k_proto.split('[')[1]
+    top_k_proto = top_k_proto.split(']')[0]
+    top_k_proto = [i for i in top_k_proto.split(',')]
+    # print(top_k_proto)
+    
+    # Count the number of prototypes that are equal to image class
+    count = 0
+    
+    for p in top_k_proto:
+        if int(p) == int(label):
+            count += 1
+    
+
+    # Update the dataframe
+    proto_stats_pr_df.iloc[[index], [-1]] = count
+
+
+# print(proto_stats_pr_df.head())
+
+
+# Open a file to save a small report w/ .TXT extension
+report = open(os.path.join("results", CHECKPOINT, "analysis", "local", "proto_stats.txt"), "at")
+
+# Get mean value of top-k cls-identity prototypes using this model
+mean_value = proto_stats_pr_df["Out-of-TopK Identity Activated Prototypes"].mean()
+# print(f"Number of prototypes per class identity: {10}")
+report.write(f"Number of prototypes per class identity: {10}\n")
+# print(f"Average number of class-identity prototypes per correctly classified image: {mean_value}")
+report.write(f"Average number of class-identity prototypes per correctly classified image: {mean_value}\n")
+
+# Close report
+report.close()
+"""
---
+++ proto_inf_stats_cub2002011.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH -p gtx1080ti_11GB                 # Partition        (check w/ $sinfo)
+#SBATCH --job-name=ga_cub2002011          # Job name
+#SBATCH -o slurm.%N.%j.out                # STDOUT
+#SBATCH -e slurm.%N.%j.err                # STDERR
+
+
+
+# CUB2002011 "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
+echo "STARTED ! CUB2002011 | Prototype Image Stats"
+
+python code/prototypes_inference_stats.py --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/
+python code/prototypes_inference_stats.py --checkpoint cub2002011/densenet161/2022-09-15_16-14-45/
+python code/prototypes_inference_stats.py --checkpoint cub2002011/resnet34/2022-09-17_17-03-33/
+# python code/prototypes_images_stats.py --dataset CUB2002011 --base_architecture resnet152 --checkpoint cub2002011/resnet152/2022-08-19_18-07-45/
+# python code/prototypes_images_stats.py --dataset CUB2002011 --base_architecture vgg16 --checkpoint cub2002011/vgg16/2022-08-23_18-52-48/
+# python code/prototypes_images_stats.py --dataset CUB2002011 --base_architecture vgg19 --checkpoint cub2002011/vgg19/2022-08-26_08-12-03/
+
+echo "FINISHED ! CUB2002011 | Prototype Image Stats"
