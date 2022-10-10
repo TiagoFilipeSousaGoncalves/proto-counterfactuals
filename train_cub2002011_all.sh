28278858927552d78c3e5f6c39bf42d1300832b0
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-10-11 00:25:36 +0100

    Added new train partition.

--- train_cub2002011_all.sh
+++ train_cub2002011_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#
-#SBATCH -p gtx1080ti_11GB                 # Partition        (check w/ $sinfo)
+
+#SBATCH -p titanxp_12GB                 # Partition        (check w/ $sinfo)
 #SBATCH --job-name=tr_cub                 # Job name
 #SBATCH -o slurm.%N.%j.out                # STDOUT
 #SBATCH -e slurm.%N.%j.err                # STDERR
