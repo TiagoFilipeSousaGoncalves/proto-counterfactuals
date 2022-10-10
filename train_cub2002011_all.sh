3560379346075469c03d40927c9ddca766c7d0f1
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-10-10 14:10:25 +0100

    Updated repository.

--- train_cub2002011_all.sh
+++ train_cub2002011_all.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+#SBATCH -p gtx1080ti_11GB                 # Partition        (check w/ $sinfo)
+#SBATCH --job-name=tr_cub                 # Job name
+#SBATCH -o slurm.%N.%j.out                # STDOUT
+#SBATCH -e slurm.%N.%j.err                # STDERR
 
 
 
@@ -6,9 +11,9 @@
 
 echo "Submitting jobs"
 
-sbatch train_cub2002011_densenet121.sh
-sbatch train_cub2002011_densenet161.sh
-sbatch train_cub2002011_resnet34.sh
+# sbatch train_cub2002011_densenet121.sh
+# sbatch train_cub2002011_densenet161.sh
+# sbatch train_cub2002011_resnet34.sh
 sbatch train_cub2002011_resnet152.sh
 sbatch train_cub2002011_vgg16.sh
 sbatch train_cub2002011_vgg19.sh
