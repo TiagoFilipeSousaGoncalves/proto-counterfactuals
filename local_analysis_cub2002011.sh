46c4f8815fd2e7d5bf79584ab924f0975fcc6a73
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-10-19 09:59:05 +0100

    Added several bash files for SLURM and for further tests.

--- local_analysis_cub2002011.sh
+++ local_analysis_cub2002011.sh
@@ -8,13 +8,13 @@
 
 
 # CUB2002011 "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
-echo "CUB2002011"
+echo "STARTED | CUB2002011 | LOCAL ANALYSIS"
 
 # python code/models_local_analysis.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/
 # python code/models_local_analysis.py --dataset CUB2002011 --base_architecture densenet161 --batchsize 16 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/densenet161/2022-09-15_16-14-45
 # python code/models_local_analysis.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 16 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/resnet34/2022-09-17_17-03-33/
-python code/models_local_analysis.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet152/
+# python code/models_local_analysis.py --dataset CUB2002011 --base_architecture resnet152 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/resnet152/
 # python code/models_local_analysis.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 16 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/vgg16/2022-10-14_21-14-35/
-python code/models_local_analysis.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --num_workers 3 --gpu_id 0 --checkpoint cub2002011/vgg19/
+python code/models_local_analysis.py --dataset CUB2002011 --base_architecture vgg19 --batchsize 16 --num_workers 0 --gpu_id 0 --checkpoint cub2002011/vgg19/2022-10-17_08-13-40/
 
-echo "Finished."
+echo "FINISHED | CUB2002011 | LOCAL ANALYSIS"
--- proto_inf_stats_cub2002011.sh
+++ proto_inf_stats_cub2002011.sh
@@ -8,13 +8,13 @@
 
 
 # CUB2002011 "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
-echo "STARTED | CUB2002011 | Prototype Image Stats"
+echo "STARTED | CUB2002011 | Prototype Inference Stats"
 
 # python code/prototypes_inference_stats.py --checkpoint cub2002011/densenet121/2022-09-14_06-32-51/
 # python code/prototypes_inference_stats.py --checkpoint cub2002011/densenet161/2022-09-15_16-14-45/
 # python code/prototypes_inference_stats.py --checkpoint cub2002011/resnet34/2022-09-17_17-03-33/
-python code/prototypes_images_stats.py --dataset CUB2002011 --base_architecture resnet152 --checkpoint cub2002011/resnet152/2022-08-19_18-07-45/
-# python code/prototypes_images_stats.py --checkpoint cub2002011/vgg16/2022-10-14_21-14-35/
-python code/prototypes_images_stats.py --dataset CUB2002011 --base_architecture vgg19 --checkpoint cub2002011/vgg19/2022-08-26_08-12-03/
+# python code/prototypes_inference_stats.py --checkpoint cub2002011/resnet152/
+# python code/prototypes_inference_stats.py --checkpoint cub2002011/vgg16/2022-10-14_21-14-35/
+python code/prototypes_inference_stats.py --checkpoint cub2002011/vgg19/2022-10-17_08-13-40/
 
-echo "FINISHED | CUB2002011 | Prototype Image Stats"
+echo "FINISHED | CUB2002011 | Prototype Inference Stats"
