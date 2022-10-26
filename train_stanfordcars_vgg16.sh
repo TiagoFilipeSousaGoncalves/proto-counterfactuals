6054cbe43d1aa6c854fc5bac12db49b800283149
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-10-26 11:45:38 +0100

    Added VGG16 Stanford Cars

--- train_stanfordcars_vgg16.sh
+++ train_stanfordcars_vgg16.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH -p rtx2080ti_11GB
+#SBATCH -p titanxp_12GB
 #SBATCH --job-name=tr_stan_vgg16          # Job name
 #SBATCH -o slurm.%N.%j.out                # STDOUT
 #SBATCH -e slurm.%N.%j.err                # STDERR
@@ -9,6 +9,6 @@
 
 echo "STANFORDCARS VGG16"
 
-python code/models_train.py --dataset STANFORDCARS --base_architecture vgg16 --batchsize 32 --num_workers 0 --gpu_id 0
+python code/models_train.py --dataset STANFORDCARS --base_architecture vgg16 --batchsize 64 --num_workers 0 --gpu_id 0
 
 echo "Finished"
