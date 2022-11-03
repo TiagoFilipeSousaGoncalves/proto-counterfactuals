1dc3d956a0f3b5cb628e213ea945e58964e7c7fe
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-11-03 11:16:23 +0000

    Updated: local_analysis_stanfordcars.sh, proto_img_stats_stanfordcars.sh, proto_inf_stats_stanfordcars.sh; Added: inference_and_proto_cub2002011.sh, inference_and_proto_stanford.sh

--- inference_and_proto_stanford.sh
+++ inference_and_proto_stanford.sh
@@ -5,7 +5,7 @@
 # STANFORDCARS "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"
 echo "STARTED | STANFORDCARS | INFERENCE AND PROTOTYPES"
 
-python code/models_inference_and_prototypes.py --dataset STANFORDCARS --base_architecture densenet121 --num_workers 2 --gpu_id 0 --checkpoint --checkpoint stanfordcars/densenet121/2022-10-24_08-55-48/
+python code/models_inference_and_prototypes.py --dataset STANFORDCARS --base_architecture densenet121 --num_workers 2 --gpu_id 0 --checkpoint stanfordcars/densenet121/2022-10-24_08-55-48/
 # python code/models_inference_and_prototypes.py --dataset STANFORDCARS --base_architecture densenet161 --num_workers 2 --gpu_id 0 --checkpoint stanfordcars/densenet161/
 python code/models_inference_and_prototypes.py --dataset STANFORDCARS --base_architecture resnet34 --num_workers 2 --gpu_id 0 --checkpoint stanfordcars/resnet34/2022-10-25_14-20-40/
 # python code/models_inference_and_prototypes.py --dataset STANFORDCARS --base_architecture resnet152 --num_workers 2 --gpu_id 0 --checkpoint stanfordcars/resnet152/
