f49ad743e7ddd3cbab5a87170285f8348324e9e2
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-11-10 12:07:53 +0000

    Updated:train_cub2002011_densenet121.sh

--- train_cub2002011_densenet121.sh
+++ train_cub2002011_densenet121.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH -p gtx1080ti_11GB                 # Partition
+#SBATCH -p gtx1080_8GB                    # Partition
 #SBATCH --job-name=ph2_d121               # Job name
 #SBATCH -o slurm.%N.%j.out                # STDOUT
 #SBATCH -e slurm.%N.%j.err                # STDERR
@@ -15,6 +15,6 @@ echo "CUB2002011 | START"
 
 # Deformable-ProtoPNet
 echo "Deformable-ProtoPNet | DenseNet121"
-python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
 
 echo "CUB2002011 | FINISHED"
