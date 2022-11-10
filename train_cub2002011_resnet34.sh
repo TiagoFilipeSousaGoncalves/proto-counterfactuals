8c388857c2c67ecfac8e11fb6e4c2002a36456d5
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-11-10 15:09:56 +0000

    Added more train scripts

--- train_cub2002011_resnet34.sh
+++ train_cub2002011_resnet34.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #
-#SBATCH -p gtx1080ti_11GB                 # Partition        (check w/ $sinfo)
-#SBATCH --job-name=tr_cub                 # Job name
-#SBATCH -o slurm.%N.%j.out                # STDOUT
-#SBATCH -e slurm.%N.%j.err                # STDERR
+#SBATCH -p gtx1080ti_11GB                   # Partition
+#SBATCH --job-name=cub_r34                  # Job name
+#SBATCH -o slurm.%N.%j.out                  # STDOUT
+#SBATCH -e slurm.%N.%j.err                  # STDERR
 
 
-echo "CUB2002011 ResNet34"
 
-python code/models_train.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --num_workers 0 --gpu_id 0
+echo "CUB2002011 | START"
 
-echo "Finished"
+# ProtoPNet
+# echo "ProtoPNet | ResNet34"
+# python code/models_train.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --num_workers 0 --gpu_id 0
+
+# Deformable-ProtoPNet
+echo "Deformable-ProtoPNet | ResNet34"
+python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+
+echo "CUB2002011 | FINISHED"
