5ff958ccc999eb5607bb5404d83e533e6dfd8556
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-11-10 14:28:33 +0000

    Updated: code/protopnet_deform/models_train.py

--- train_cub2002011_densenet121.sh
+++ train_cub2002011_densenet121.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH -p gtx1080ti_11GB                   # Partition
-#SBATCH --job-name=ph2_d121                 # Job name
+#SBATCH --job-name=cub_d121                 # Job name
 #SBATCH -o slurm.%N.%j.out                  # STDOUT
 #SBATCH -e slurm.%N.%j.err                  # STDERR
 
