eab6d63cf7133f65fdf25cd9d34a899e3801b86d
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-11-14 07:43:45 +0000

    Updated: train_cub2002011_vgg16.sh

--- train_cub2002011_vgg16.sh
+++ train_cub2002011_vgg16.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #
-#SBATCH -p titanxp_12GB                   # Partition        (check w/ $sinfo)
-#SBATCH --job-name=tr_cub                 # Job name
-#SBATCH -o slurm.%N.%j.out                # STDOUT
-#SBATCH -e slurm.%N.%j.err                # STDERR
+#SBATCH -p gtx1080ti_11GB                   # Partition
+#SBATCH --job-name=cub_v16                  # Job name
+#SBATCH -o slurm.%N.%j.out                  # STDOUT
+#SBATCH -e slurm.%N.%j.err                  # STDERR
 
 
 echo "CUB2002011 VGG16"
 
-# NUM_WORKERS=$(nproc)
 
-python code/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 64 --num_workers 0 --gpu_id 0
+# ProtoPNet
+# echo "ProtoPNet | VGG16"
+# python code/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 64 --num_workers 0 --gpu_id 0
 
-echo "Finished"
+# Deformable-ProtoPNet
+echo "Deformable-ProtoPNet | VGG16"
+python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 32 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+
+echo "CUB2002011 | FINISHED"
