9accc6ca885fb3ff2ede2011b45fcc80ee553915
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-11-22 09:52:27 +0000

    Updated train_ph2_resnet152.sh, train_ph2_resnet34.sh

--- train_ph2_resnet152.sh
+++ train_ph2_resnet152.sh
@@ -14,7 +14,7 @@ echo "PH2 | STARTED"
 # python code/models_train.py --dataset PH2 --base_architecture resnet152 --batchsize 16 --num_workers 0 --gpu_id 0
 
 # Deformable-ProtoPNet
-echo "Deformable-ProtoPNet | DenseNet161"
+echo "Deformable-ProtoPNet | ResNet152"
 python code/protopnet_deform/models_train.py --dataset PH2 --base_architecture resnet152 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
 
 echo "PH2 | FINISHED"
