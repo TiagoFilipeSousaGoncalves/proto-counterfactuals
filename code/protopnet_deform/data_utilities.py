5cbd53bc0001532f28e51518ba186bec8a493586
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-11-30 13:14:47 +0000

    Added several updates for the prototype retrieval modules of the deformable ProtoPNet

--- code/protopnet_deform/data_utilities.py
+++ code/protopnet_deform/data_utilities.py
@@ -79,7 +79,196 @@ def undo_preprocess_input_function(x, mean=(0.485, 0.456, 0.406), std=(0.229, 0.
 
 
 
-# Dataset
+# Function: Save preprocessed image(s)
+def save_preprocessed_img(fname, preprocessed_imgs, index=0):
+    
+    # Make a copy of the image
+    img_copy = copy.deepcopy(preprocessed_imgs[index:index+1])
+    
+    # Revert any type of preprocessing
+    undo_preprocessed_img = undo_preprocess_input_function(img_copy)
+    
+    # FIXME: Erase after review
+    # print('image index {0} in batch'.format(index))
+    
+    # Get unprocessed version of the image
+    undo_preprocessed_img = undo_preprocessed_img[0]
+    undo_preprocessed_img = undo_preprocessed_img.detach().cpu().numpy()
+    undo_preprocessed_img = np.transpose(undo_preprocessed_img, [1,2,0])
+
+    # Save image    
+    plt.imsave(fname, undo_preprocessed_img)
+
+
+    return undo_preprocessed_img
+
+
+
+# Function: Save image prototypes
+def save_prototype(fname, load_img_dir, index):
+    
+    # Open an image file and save it
+    p_img = plt.imread(os.path.join(load_img_dir, 'prototype-img'+str(index)+'.png'))
+    plt.imsave(fname, p_img)
+
+    return
+    
+
+
+# Function: Save prototype bbox
+def save_prototype_box(fname, load_img_dir, index):
+    
+    # Open an image file and save it
+    p_img = plt.imread(os.path.join(load_img_dir, 'prototype-img-with_box'+str(index)+'.png'))
+    plt.imsave(fname, p_img)
+
+    return
+    
+
+
+# Function: Save image with a bounding-box
+def imsave_with_bbox(fname, img_rgb, bbox_height_start, bbox_height_end, bbox_width_start, bbox_width_end, color=(0, 255, 255)):
+    
+    # Convert RGB to BGR with OpenCV
+    img_bgr_uint8 = cv2.cvtColor(np.uint8(255*img_rgb), cv2.COLOR_RGB2BGR)
+    
+    # Draw rectangle around bbox
+    cv2.rectangle(img_bgr_uint8, (bbox_width_start, bbox_height_start), (bbox_width_end-1, bbox_height_end-1), color, thickness=2)
+    
+    # Convert into RGB and type float
+    img_rgb_uint8 = img_bgr_uint8[...,::-1]
+    img_rgb_float = np.float32(img_rgb_uint8) / 255
+    
+    # Save image
+    plt.imsave(fname, img_rgb_float)
+
+
+    return
+
+
+
+# Function: Save deformable prototype information
+def save_deform_info(model, offsets, input, activations, save_dir, prototype_img_filename_prefix, proto_index, prototype_layer_stride):
+    
+    # Get prototype shape
+    prototype_shape = model.prototype_shape
+
+    # FIXME: Review this piece of code
+    if not hasattr(model, "prototype_dilation"):
+        dilation = model.prototype_dillation
+    else:
+        dilation = model.prototype_dilation
+    
+    
+    # Get input shape
+    original_img_size = input.shape[0]
+
+    colors = [
+        (230/255, 25/255, 75/255),
+        (60/255, 180/255, 75/255),
+        (255/255, 225/255, 25/255),
+        (0, 130/255, 200/255),
+        (245/255, 130/255, 48/255),
+        (70/255, 240/255, 240/255),
+        (240/255, 50/255, 230/255),
+        (170/255, 110/255, 40/255), (0,0,0)
+    ]
+
+
+    # Get Prototype Activations
+    argmax_proto_act_j = list(np.unravel_index(np.argmax(activations, axis=None), activations.shape))
+    fmap_height_start_index = argmax_proto_act_j[0] * prototype_layer_stride
+    fmap_width_start_index = argmax_proto_act_j[1] * prototype_layer_stride
+
+
+    # Get original image(s) with bboxes
+    original_img_j_with_boxes = input.copy()
+
+
+    # Get number of deformable groups (model.num_prototypes // model.num_classes)
+    num_def_groups = 1
+
+    # Get deformable group(s) indices and offset
+    def_grp_index = proto_index % num_def_groups
+    def_grp_offset = def_grp_index * 2 * prototype_shape[-2] * prototype_shape[-1]
+
+
+    # Iterate through prototype shape(s) to get the prototypes
+    for i in range(prototype_shape[-2]):
+        for k in range(prototype_shape[-1]):
+            
+            # Offsets go in order height offset, width offset
+            h_index = def_grp_offset + 2 * (k + prototype_shape[-2]*i)
+            w_index = h_index + 1
+            h_offset = offsets[0, h_index, fmap_height_start_index, fmap_width_start_index]
+            w_offset = offsets[0, w_index, fmap_height_start_index, fmap_width_start_index]
+
+            # Subtract prototype_shape // 2 because fmap start indices give the center location, and we start with top left
+            def_latent_space_row = fmap_height_start_index + h_offset + (i - prototype_shape[-2] // 2) * dilation[0]
+            def_latent_space_col = fmap_width_start_index + w_offset + (k - prototype_shape[-1] // 2)* dilation[1]
+
+            # Map the coordinates above into the image under analysis
+            def_image_space_row_start = int(def_latent_space_row * original_img_size / activations.shape[-2])
+            def_image_space_row_end = int((1 + def_latent_space_row) * original_img_size / activations.shape[-2])
+            def_image_space_col_start = int(def_latent_space_col * original_img_size / activations.shape[-1])
+            def_image_space_col_end = int((1 + def_latent_space_col) * original_img_size / activations.shape[-1])
+
+
+            # Get the image with this bbox
+            img_with_just_this_box = input.copy()
+            
+            # Draw rectangle 
+            cv2.rectangle(
+                img_with_just_this_box,
+                (def_image_space_col_start, def_image_space_row_start),
+                (def_image_space_col_end, def_image_space_row_end),
+                colors[i*prototype_shape[-1] + k],
+                1
+            )
+
+
+            # Save this image
+            plt.imsave(
+                os.path.join(save_dir, prototype_img_filename_prefix + str(proto_index) + '_patch_' + str(i*prototype_shape[-1] + k) + '-with_box.png'),
+                img_with_just_this_box,
+                vmin=0.0,
+                vmax=1.0
+            )
+
+
+            # Draw rectangle ont the other image with bboxes
+            cv2.rectangle(
+                original_img_j_with_boxes,
+                (def_image_space_col_start, def_image_space_row_start),
+                (def_image_space_col_end, def_image_space_row_end),
+                colors[i*prototype_shape[-1] + k],
+                1
+            )
+            
+
+            # Save this image uppon a given condition (defined by the original authors)
+            if not (def_image_space_col_start < 0 or def_image_space_row_start < 0 or def_image_space_col_end >= input.shape[0] or def_image_space_row_end >= input.shape[1]):
+                plt.imsave(
+                    os.path.join(save_dir, prototype_img_filename_prefix + str(proto_index) + '_patch_' + str(i*prototype_shape[-1] + k) + '.png'),
+                    input[def_image_space_row_start:def_image_space_row_end, def_image_space_col_start:def_image_space_col_end, :],
+                    vmin=0.0,
+                    vmax=1.0
+                )
+
+
+    # Save this image
+    plt.imsave(
+        os.path.join(save_dir, prototype_img_filename_prefix + str(proto_index) + '-with_box.png'),
+        original_img_j_with_boxes,
+        vmin=0.0,
+        vmax=1.0
+    )
+
+    return
+
+
+
+#  Dataset
 # STANFORDCARSDataset: Dataset Class
 class STANFORDCARSDataset(Dataset):
     def __init__(self, data_path, cars_subset, augmented, cropped=True, transform=None):
