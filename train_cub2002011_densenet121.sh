8c9a13afd922c08af8d62699be26fdb6f8cd91f0
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-12-05 13:57:09 +0000

    Changed several scripts and started a new training batch more complete

--- train_cub2002011_densenet121.sh
+++ train_cub2002011_densenet121.sh
@@ -11,10 +11,10 @@ echo "CUB2002011 | START"
 
 # ProtoPNet
 echo "ProtoPNet | DenseNet121"
-python code/protopnet/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --num_workers 0 --gpu_id 0
+python code/protopnet/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --num_workers 0 --gpu_id 0
 
 # Deformable-ProtoPNet
 # echo "Deformable-ProtoPNet | DenseNet121"
-# python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+# python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 32 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
 
 echo "CUB2002011 | FINISHED"
