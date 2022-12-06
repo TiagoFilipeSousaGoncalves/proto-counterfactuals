dc44406929c753286a50d8987c79568599492878
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-12-06 00:45:30 +0000

    modified: train_cub2002011_densenet121.sh

--- train_cub2002011_densenet121.sh
+++ train_cub2002011_densenet121.sh
@@ -10,11 +10,11 @@
 echo "CUB2002011 | START"
 
 # ProtoPNet
-# echo "ProtoPNet | DenseNet121"
-# python code/protopnet/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --num_workers 0 --gpu_id 0
+echo "ProtoPNet | DenseNet121"
+python code/protopnet/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --num_workers 0 --gpu_id 0
 
 # Deformable-ProtoPNet
-echo "Deformable-ProtoPNet | DenseNet121"
-python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+# echo "Deformable-ProtoPNet | DenseNet121"
+# python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
 
 echo "CUB2002011 | FINISHED"
