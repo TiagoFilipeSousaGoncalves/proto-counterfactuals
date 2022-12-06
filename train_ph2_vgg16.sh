b9dc89c8d148d9d8247b4755641bae28c0aa50ac
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-12-06 16:12:37 +0000

    Added ProtoPNet to PH2

--- train_ph2_vgg16.sh
+++ train_ph2_vgg16.sh
@@ -14,7 +14,7 @@ echo "ProtoPNet | VGG16"
 python code/protopnet/models_train.py --dataset PH2 --base_architecture vgg16 --batchsize 16 --num_workers 0 --gpu_id 0
 
 # Deformable-ProtoPNet
-# echo "Deformable-ProtoPNet | ResNet152"
+# echo "Deformable-ProtoPNet | VGG16"
 # python code/protopnet_deform/models_train.py --dataset PH2 --base_architecture vgg16 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
 
 echo "PH2 | FINISHED"
