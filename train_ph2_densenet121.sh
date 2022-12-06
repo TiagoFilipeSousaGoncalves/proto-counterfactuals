fd660d77a4f11e5ea86163c67e7e8058b37decac
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-12-06 15:49:37 +0000

    modified:   train_ph2_densenet121.sh

--- train_ph2_densenet121.sh
+++ train_ph2_densenet121.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH -p gtx1080ti_11GB                 # Partition
+#SBATCH -p rtx2080ti_11GB                 # Partition
 #SBATCH --job-name=ph2_d121               # Job name
 #SBATCH -o slurm.%N.%j.out                # STDOUT
 #SBATCH -e slurm.%N.%j.err                # STDERR
@@ -11,10 +11,10 @@ echo "PH2 | STARTED"
 
 # ProtoPNet
 echo "ProtoPNet | DenseNet121"
-python code/models_train.py --dataset PH2 --base_architecture densenet121 --batchsize 32 --num_workers 0 --gpu_id 0
+python code/models_train.py --dataset PH2 --base_architecture densenet121 --batchsize 16 --num_workers 0 --gpu_id 0
 
 # Deformable-ProtoPNet
 # echo "Deformable-ProtoPNet | DenseNet121"
-# python code/protopnet_deform/models_train.py --dataset PH2 --base_architecture densenet121 --batchsize 32 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+# python code/protopnet_deform/models_train.py --dataset PH2 --base_architecture densenet121 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
 
 echo "PH2 | FINISHED"
