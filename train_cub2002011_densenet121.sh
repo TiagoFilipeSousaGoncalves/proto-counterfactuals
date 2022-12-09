15f036a8f91b3ebf6541caa5858cec18a03495e8
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-12-09 16:38:15 +0000

    Added new training scripts for cub

--- train_cub2002011_densenet121.sh
+++ train_cub2002011_densenet121.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH -p titanxp_12GB                     # Partition
+#SBATCH -p gtx1080ti_11GB                   # Partition
 #SBATCH --job-name=cub_d121                 # Job name
 #SBATCH -o slurm.%N.%j.out                  # STDOUT
 #SBATCH -e slurm.%N.%j.err                  # STDERR
