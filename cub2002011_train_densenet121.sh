5aef450afefa72d28dc3743fa635a3af7d6e5191
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-12-20 11:09:55 +0000

    Added CUB2002011 training scripts

--- cub2002011_train_densenet121.sh
+++ cub2002011_train_densenet121.sh
@@ -9,11 +9,13 @@
 
 echo "Started | CUB2002011 | Training"
 
-if [ $1 == "ppnet" ]
+model="ppnet"
+
+if [ $model == "ppnet" ]
 then
 echo "CUB200211 | ProtoPNet DenseNet121"
     python code/protopnet/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 128 --num_workers 0 --gpu_id 0
-elif [ $1 == 'dppnet' ]
+elif [ $model == 'dppnet' ]
 then
     echo "CUB200211 | Deformable-ProtoPNet DenseNet121"
     python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture densenet121 --batchsize 128 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
