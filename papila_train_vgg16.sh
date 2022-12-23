1409e0176940f4051f7432f9ea3882ad17cf92ed
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-12-23 17:58:59 +0000

    papila_train_vgg16.sh

---
+++ papila_train_vgg16.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#SBATCH -p teslav100_32GB                 # Partition
+#SBATCH --job-name=pap_v16                # Job name
+#SBATCH -o slurm.%N.%j.out                # STDOUT
+#SBATCH -e slurm.%N.%j.err                # STDERR
+
+
+
+echo "PAPILA | Started | Training"
+
+model="ppnet"
+
+if [ $model == "ppnet" ]
+then
+    echo "ProtoPNet | ResNet34"
+    python code/protopnet/models_train.py --dataset PAPILA --base_architecture vgg16 --batchsize 64 --num_workers 0 --gpu_id 0
+elif [ $model == 'dppnet' ]
+then
+    echo "Deformable-ProtoPNet | ResNet34"
+    python code/protopnet_deform/models_train.py --dataset PAPILA --base_architecture vgg16 --batchsize 64 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+else
+    echo "Error"
+fi
+
+echo "PAPILA | Finished | Training"
