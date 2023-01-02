0af26bc27a6f7eb0d76f774b1b7ceb5353c7a456
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2023-01-02 08:45:17 +0000

    Updated: ph2_train_resnet34.sh

--- ph2_train_resnet34.sh
+++ ph2_train_resnet34.sh
@@ -7,14 +7,20 @@
 
 
 
-echo "PH2 | STARTED"
+echo "Started | PH2 | Training"
 
-# ProtoPNet
-# echo "ProtoPNet | ResNet34"
-# python code/protopnet/models_train.py --dataset PH2 --base_architecture resnet34 --batchsize 16 --num_workers 0 --gpu_id 0
+model="dppnet"
 
-# Deformable-ProtoPNet
-echo "Deformable-ProtoPNet | ResNet34"
-python code/protopnet_deform/models_train.py --dataset PH2 --base_architecture resnet34 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+if [ $model == "ppnet" ]
+then
+    echo "PH2 | ProtoPNet ResNet34"
+    python code/protopnet/models_train.py --dataset PH2 --base_architecture resnet34 --batchsize 16 --num_workers 0 --gpu_id 0
+elif [ $model == 'dppnet' ]
+then
+    echo "PH2 | Deformable-ProtoPNet ResNet34"
+    python code/protopnet_deform/models_train.py --dataset PH2 --base_architecture resnet34 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+else
+    echo "Error"
+fi
 
-echo "PH2 | FINISHED"
+echo "Finished | PH2 | Training"
