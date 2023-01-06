4c3aad39bdadbaeb70708b4cbfcb4ca5ffadc340
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2023-01-06 18:26:50 +0000

    Bug fixed on training loop of PPNet

--- cub2002011_train_resnet34.sh
+++ cub2002011_train_resnet34.sh
@@ -14,11 +14,11 @@ model="dppnet"
 if [ $model == "ppnet" ]
 then
     echo "CUB200211 | ProtoPNet ResNet34"
-    python code/protopnet/models_train.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 128 --num_workers 0 --gpu_id 0
+    python code/protopnet/models_train.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --num_workers 0 --gpu_id 0
 elif [ $model == 'dppnet' ]
 then
     echo "CUB200211 | Deformable-ProtoPNet ResNet34"
-    python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 64 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
+    python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture resnet34 --batchsize 32 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
 else
     echo "Error"
 fi
