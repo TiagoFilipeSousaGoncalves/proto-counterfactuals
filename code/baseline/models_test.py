e41da60f8d086fbf364dfb85780312a52cbe1caa
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2023-01-10 11:59:23 +0000

    Updated the code of baseline models

--- code/baseline/models_test.py
+++ code/baseline/models_test.py
@@ -15,7 +15,7 @@ np.random.seed(random_seed)
 
 # Project Imports
 from data_utilities import CUB2002011Dataset, PAPILADataset, PH2Dataset, STANFORDCARSDataset
-from model_utilities import construct_PPNet
+from model_utilities import DenseNet, ResNet, VGG
 from train_val_test_utilities import model_test
 
 
@@ -32,7 +32,7 @@ parser.add_argument('--data_dir', type=str, default="data", help="Directory of t
 parser.add_argument('--dataset', type=str, required=True, choices=["CUB2002011", "PAPILA", "PH2", "STANFORDCARS"], help="Data set: CUB2002011, PAPILA, PH2, STANFORDCARS.")
 
 # Model
-parser.add_argument('--base_architecture', type=str, required=True, choices=["densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"], help='Base architecture: densenet121, resnet18, vgg19.')
+parser.add_argument('--base_architecture', type=str, required=True, choices=["densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19"], help='Base architecture: "densenet121", "densenet161", "resnet34", "resnet152", "vgg16", "vgg19".')
 
 # Batch size
 parser.add_argument('--batchsize', type=int, default=4, help="Batch-size for training and validation")
@@ -40,14 +40,6 @@ parser.add_argument('--batchsize', type=int, default=4, help="Batch-size for tra
 # Image size
 # img_size = 224
 parser.add_argument('--img_size', type=int, default=224, help="Size of the image after transforms")
-
-# Prototype Activation Function
-# prototype_activation_function = 'log'
-parser.add_argument('--prototype_activation_function', type=str, default='log', help="Prototype activation function.")
-
-# Add on layers type
-# add_on_layers_type = 'regular'
-parser.add_argument('--add_on_layers_type', type=str, default='regular', help="Add on layers type.")
 
 # Output directory
 parser.add_argument("--output_dir", type=str, default="results", help="Output directory.")
@@ -85,17 +77,11 @@ OUTPUT_DIR = args.output_dir
 # Number of workers (threads)
 WORKERS = args.num_workers
 
-# Prototype activation function
-PROTOTYPE_ACTIVATION_FUNCTION = args.prototype_activation_function
-
 # Batch size
 BATCH_SIZE = args.batchsize
 
 # Image size (after transforms)
 IMG_SIZE = args.img_size
-
-# Add on layers type
-ADD_ON_LAYERS_TYPE = args.add_on_layers_type
 
 
 
@@ -302,44 +288,17 @@ print(f"Using device: {DEVICE}")
 
 
 
-# Define prototype shape according to original paper
-# The number of prototypes can be chosen with prior domain knowledge or hyperparameter search: we used 10 prototypes per class
-NUM_PROTOTYPES_CLASS = int(NUM_CLASSES * 10)
-
-# For VGG-16, VGG-19, DenseNet-121, DenseNet-161, we used 128 as the number of channels in a prototype
-if BASE_ARCHITECTURE.lower() in ("densenet121", "densenet161", "vgg16", "vgg19"):
-    PROTOTYPE_SHAPE = (NUM_PROTOTYPES_CLASS, 128, 1, 1)
-
-# For ResNet-34, we used 256 as the number of channels in a prototype;
-elif BASE_ARCHITECTURE.lower() in ("resnet34"):
-    PROTOTYPE_SHAPE = (NUM_PROTOTYPES_CLASS, 256, 1, 1)
-
-# For ResNet-152, we used 512 as the number of channels in a prototype
-elif BASE_ARCHITECTURE.lower() in ("resnet152"):
-    PROTOTYPE_SHAPE = (NUM_PROTOTYPES_CLASS, 512, 1, 1)
-
-
-
-
 # Construct the Model
-ppnet_model = construct_PPNet(
-    base_architecture=BASE_ARCHITECTURE,
-    pretrained=False,
-    img_size=IMG_SIZE,
-    prototype_shape=PROTOTYPE_SHAPE,
-    num_classes=NUM_CLASSES,
-    prototype_activation_function=PROTOTYPE_ACTIVATION_FUNCTION,
-    add_on_layers_type=ADD_ON_LAYERS_TYPE
-)
-
-
-# Define if the model should be class specific
-class_specific = True
-
+if BASE_ARCHITECTURE.lower() in ("densenet121", "densenet161"):
+    baseline_model = DenseNet(backbone=BASE_ARCHITECTURE.lower(), channels=3, height=IMG_SIZE, width=IMG_SIZE, nr_classes=NUM_CLASSES)
+elif BASE_ARCHITECTURE.lower() in ("resnet34", "resnet152"):
+    baseline_model = ResNet(backbone=BASE_ARCHITECTURE.lower(), channels=3, height=IMG_SIZE, width=IMG_SIZE, nr_classes=NUM_CLASSES)
+else:
+    baseline_model = VGG(backbone=BASE_ARCHITECTURE.lower(), channels=3, height=IMG_SIZE, width=IMG_SIZE, nr_classes=NUM_CLASSES)
 
 
 # Put model into DEVICE (CPU or GPU)
-ppnet_model = ppnet_model.to(DEVICE)
+baseline_model = baseline_model.to(DEVICE)
 
 
 # Load model weights
@@ -359,16 +318,16 @@ report = open(os.path.join(results_dir, "acc_report.txt"), "at")
 # Iterate through these model weights types
 for model_fname in [model_path, model_path_push, model_path_push_last]:
     model_weights = torch.load(model_fname, map_location=DEVICE)
-    ppnet_model.load_state_dict(model_weights['model_state_dict'], strict=True)
+    baseline_model.load_state_dict(model_weights['model_state_dict'], strict=True)
     report.write(f"Model weights loaded with success from {model_fname}.\n")
 
 
     # Get performance metrics
-    train_metrics_dict = model_test(model=ppnet_model, dataloader=train_loader, device=DEVICE, class_specific=class_specific)
+    train_metrics_dict = model_test(model=baseline_model, dataloader=train_loader, device=DEVICE)
     train_acc = train_metrics_dict["accuracy"]
     report.write(f"Train Accuracy: {train_acc}\n")
 
-    test_metrics_dict = model_test(model=ppnet_model, dataloader=test_loader, device=DEVICE, class_specific=class_specific)
+    test_metrics_dict = model_test(model=baseline_model, dataloader=test_loader, device=DEVICE)
     test_accuracy = test_metrics_dict["accuracy"]
     report.write(f"Test Accuracy: {test_accuracy}\n")
 
