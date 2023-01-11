60bedfb7e76d0ae659fc989121eb5a6ef2fb9dc6
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2023-01-11 19:06:30 +0000

    Updated cub2002011_train_vgg16.sh

--- cub2002011_train_vgg16.sh
+++ cub2002011_train_vgg16.sh
@@ -15,7 +15,7 @@ if [ $model == "ppnet" ]
 then
     echo "CUB200211 | ProtoPNet VGG16"
     python code/protopnet/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 32 --num_workers 0 --gpu_id 0
-elif [ $model == 'dppnet' ]
+elif [ $model == "dppnet" ]
 then
     echo "CUB200211 | Deformable-ProtoPNet VGG16"
     python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 32 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
