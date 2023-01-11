8434e567bbc33f36c8456176462870f4760bf622
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2023-01-11 19:00:58 +0000

    Updated: papila_train_densenet121.sh, papila_train_resnet34.sh, papila_train_vgg16.sh

--- papila_train_resnet34.sh
+++ papila_train_resnet34.sh
@@ -9,13 +9,17 @@
 
 echo "PAPILA | Started | Training"
 
-model="dppnet"
+model="baseline"
 
-if [ $model == "ppnet" ]
+if [ $model == "baseline" ]
+then
+    echo "Baseline | ResNet34"
+    python code/baseline/models_train.py --dataset PAPILA --base_architecture resnet34 --batchsize 16 --num_workers 0 --gpu_id 0
+elif [ $model == "ppnet" ]
 then
     echo "ProtoPNet | ResNet34"
     python code/protopnet/models_train.py --dataset PAPILA --base_architecture resnet34 --batchsize 16 --num_workers 0 --gpu_id 0
-elif [ $model == 'dppnet' ]
+elif [ $model == "dppnet" ]
 then
     echo "Deformable-ProtoPNet | ResNet34"
     python code/protopnet_deform/models_train.py --dataset PAPILA --base_architecture resnet34 --batchsize 16 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
