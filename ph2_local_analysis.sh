22c145380122a1af5a5699c2b1e4f29c9537c166
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2023-01-25 10:35:55 +0000

    Updated: ph2_local_analysis.sh

--- ph2_local_analysis.sh
+++ ph2_local_analysis.sh
@@ -17,7 +17,7 @@ then
     python code/protopnet/models_local_analysis.py --dataset PH2 --base_architecture resnet152 --num_workers 0 --gpu_id 0 --checkpoint ph2/protopnet/resnet152/2022-12-07_00-40-00/
     python code/protopnet/models_local_analysis.py --dataset PH2 --base_architecture vgg16 --num_workers 0 --gpu_id 0 --checkpoint ph2/protopnet/vgg16/2022-12-07_00-42-57/
     python code/protopnet/models_local_analysis.py --dataset PH2 --base_architecture vgg19 --num_workers 0 --gpu_id 0 --checkpoint ph2/protopnet/vgg19/2022-12-07_02-48-40/
-elif [ $model == "dppnet"]
+elif [ $model == "dppnet" ]
 then
     echo "PH2 | Deformable ProtoPNet"
     python code/protopnet_deform/models_local_analysis.py --dataset PH2 --base_architecture densenet121 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0 --checkpoint ph2/deformable-protopnet/densenet121/2023-01-02_08-43-56/
