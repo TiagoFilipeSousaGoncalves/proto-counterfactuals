27984081e65bdb87d82861ac8c36af8d70240596
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2023-02-19 17:10:41 +0000

    Updated: code/protopnet/counterfactuals_labels_coherence.py

--- code/protopnet/counterfactuals_labels_coherence.py
+++ code/protopnet/counterfactuals_labels_coherence.py
@@ -109,17 +109,20 @@ for image_filename in label_coherence_dict.keys():
         # Get the set of nearest counterfactual labels
         counterfactual_labels_among_models = label_coherence_dict[image_filename]["Nearest Counterfactual Label"]
         counterfactual_labels_among_models = np.array(counterfactual_labels_among_models, dtype=np.int)
-        print(counterfactual_labels_among_models)
-        print(counterfactual_labels_among_models.shape)
+        # print(counterfactual_labels_among_models)
+        # print(counterfactual_labels_among_models.shape)
 
-        # Transpose the vector so we have the right format to compute the Fleiss Kappa
-        counterfactual_labels_among_models = np.transpose(counterfactual_labels_among_models)
+        # Reshape the vector so we have the right format to compute the Fleiss Kappa
+        # counterfactual_labels_among_models = np.transpose(counterfactual_labels_among_models)
+        counterfactual_labels_among_models = np.reshape(counterfactual_labels_among_models, (-1, 1))
         print(counterfactual_labels_among_models.dtype)
         print(counterfactual_labels_among_models)
-        
+
         # Compute the fleiss kappa value
         fleiss_kappa_arr, categories_arr = aggregate_raters(data=counterfactual_labels_among_models, n_cat=N_CLASSES)
         fleiss_kappa_value = fleiss_kappa(table=fleiss_kappa_arr, method='uniform')
+
+        exit()
 
         # Add this to the dictionary
         label_coherence_dict[image_filename]["Counterfactual Label Coherence"] = fleiss_kappa_value
