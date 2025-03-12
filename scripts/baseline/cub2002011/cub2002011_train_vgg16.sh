a122dfddf0d266d5863e7e89b9bec3798feb8fac
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-03-12 09:24:13 +0000

    Updated: scripts/baseline/cub2002011/

--- scripts/baseline/cub2002011/cub2002011_train_vgg16.sh
+++ scripts/baseline/cub2002011/cub2002011_train_vgg16.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
-#
-#SBATCH -p gtx1080ti_11GB                 # Partition
-#SBATCH --qos=gtx1080ti                   # QOS
-#SBATCH --job-name=cub_v16                  # Job name
-#SBATCH -o slurm.%N.%j.out                  # STDOUT
-#SBATCH -e slurm.%N.%j.err                  # STDERR
+#SBATCH -p gpu_min32gb                     # Partition
+#SBATCH --qos=gpu_min32gb                       # QOS
+#SBATCH --job-name=cub_v16                # Job name
+#SBATCH -o cub_v16.out                  # STDOUT
+#SBATCH -e cubcub_v16_d121.err                  # STDERR
 
 
 
 echo "Started | CUB2002011 | Training"
 
-model="baseline"
+echo "CUB200211 | Baseline DenseNet121"
+python src/baseline/models_train.py \
+ --data_dir '/nas-ctm01/datasets/public/cub2002011-dataset' \
+ --dataset cub2002011 \
+ --base_architecture vgg16 \
+ --batchsize 32 \
+ --num_workers 4 \
+ --gpu_id 0
 
-if [ $model == "baseline" ]
-then
-    echo "CUB200211 | Baseline VGG16"
-    python code/baseline/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 32 --num_workers 0 --gpu_id 0
-elif [ $model == "ppnet" ]
-then
-    echo "CUB200211 | ProtoPNet VGG16"
-    python code/protopnet/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 32 --num_workers 0 --gpu_id 0
-elif [ $model == "dppnet" ]
-then
-    echo "CUB200211 | Deformable-ProtoPNet VGG16"
-    python code/protopnet_deform/models_train.py --dataset CUB2002011 --base_architecture vgg16 --batchsize 32 --subtractive_margin --using_deform --last_layer_fixed --num_workers 0 --gpu_id 0
-else
-    echo "Error"
-fi
-
-echo "Finished | CUB2002011 | Training"
+echo "CUB2002011 | FINISHED"
