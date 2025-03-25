dc618f7045b992d4d01dd872e2bbc0fe8be4fbe5
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-03-25 11:38:14 +0000

    Updated: scripts/deformable-protopnet/ph2

--- scripts/deformable-protopnet/ph2/ph2_train_resnet34.sh
+++ scripts/deformable-protopnet/ph2/ph2_train_resnet34.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH -p gpu_min32gb                 # Partition
-#SBATCH --qos=gpu_min32gb                   # QOS
+#SBATCH -p gpu_min11gb                 # Partition
+#SBATCH --qos=gpu_min11gb                   # QOS
 #SBATCH --job-name=ph2_r34                  # Job name
 #SBATCH -o ph2_r34.out                  # STDOUT
 #SBATCH -e ph2_r34.err                  # STDERR
@@ -15,7 +15,7 @@ python src/deformable-protopnet/models_train.py \
  --data_dir '/nas-ctm01/datasets/public/MEDICAL/ph2-database' \
  --dataset ph2 \
  --base_architecture resnet34 \
- --batchsize 32 \
+ --batchsize 16 \
  --subtractive_margin \
  --using_deform \
  --last_layer_fixed \
