aa06f5e673dee2c6528332b63b56af68e5c9b029
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-03-26 11:31:43 +0000

    Updated: scripts/deformable-protopnet/papila/papila_train_resnet34.sh, scripts/deformable-protopnet/papila/papila_train_vgg16.sh

--- scripts/deformable-protopnet/papila/papila_train_resnet34.sh
+++ scripts/deformable-protopnet/papila/papila_train_resnet34.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH -p gpu_min32gb                 # Partition
-#SBATCH --qos=gpu_min32gb                   # QOS
+#SBATCH -p gpu_min11gb                 # Partition
+#SBATCH --qos=gpu_min11gb                   # QOS
 #SBATCH --job-name=pla_r34                  # Job name
 #SBATCH -o pla_r34.out                  # STDOUT
 #SBATCH -e pla_r34.err                  # STDERR
@@ -15,7 +15,7 @@ python src/deformable-protopnet/models_train.py \
  --data_dir '/nas-ctm01/datasets/public/MEDICAL/papila-dataset-glaucoma-fundus-images' \
  --dataset papila \
  --base_architecture resnet34 \
- --batchsize 32 \
+ --batchsize 16 \
  --subtractive_margin \
  --using_deform \
  --last_layer_fixed \
