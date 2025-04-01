50aa494af524455ad6ce60c36e0fdd35820d8796
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-04-01 15:37:22 +0100

    Updated scripts/

--- scripts/protopnet/papila/papila_prototypes_inference_stats.sh
+++ scripts/protopnet/papila/papila_prototypes_inference_stats.sh
@@ -10,13 +10,13 @@
 echo "PAPILA | Started | Prototype Inference Stats"
 
 echo "PAPILA | ProtoPNet"
-python code/protopnet/prototypes_inference_stats.py \
+python src/protopnet/prototypes_inference_stats.py \
  --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/papila/protopnet/densenet121/2025-03-23_09-59-10/'
 
-python code/protopnet/prototypes_inference_stats.py \
+python src/protopnet/prototypes_inference_stats.py \
  --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/papila/protopnet/resnet34/2025-03-23_09-59-09/'
 
-python code/protopnet/prototypes_inference_stats.py \
+python src/protopnet/prototypes_inference_stats.py \
  --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/papila/protopnet/vgg16/2025-03-23_15-12-24'
 
 echo "PAPILA | Finished | Prototype Inference Stats"
