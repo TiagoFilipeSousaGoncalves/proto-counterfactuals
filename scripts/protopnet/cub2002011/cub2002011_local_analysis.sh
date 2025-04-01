cea25c24c3169bbea3827074e6d32c03c45ab418
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-04-01 23:43:34 +0100

    Updated: scripts/protopnet/cub2002011/cub2002011_local_analysis.sh

--- scripts/protopnet/cub2002011/cub2002011_local_analysis.sh
+++ scripts/protopnet/cub2002011/cub2002011_local_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -p debug_8gb                     # Partition
-#SBATCH --qos=debug_8gb                       # QOS
+#SBATCH -p gpu_min11gb                     # Partition
+#SBATCH --qos=gpu_min11gb                       # QOS
 #SBATCH --job-name=cub_lana                 # Job name
 #SBATCH -o cub_lana.out                  # STDOUT
 #SBATCH -e cub_lana.err                  # STDERR
@@ -17,20 +17,20 @@ python src/protopnet/models_local_analysis.py \
  --gpu_id 0 \
  --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/protopnet/densenet121/2025-03-14_11-19-56/'
 
-# python src/protopnet/models_local_analysis.py \
-#  --data_dir '/nas-ctm01/datasets/public/cub2002011-dataset' \
-#  --dataset cub2002011 \
-#  --base_architecture resnet34 \
-#  --num_workers 4 \
-#  --gpu_id 0 \
-#  --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/protopnet/resnet34/2025-03-17_15-17-17/'
+python src/protopnet/models_local_analysis.py \
+ --data_dir '/nas-ctm01/datasets/public/cub2002011-dataset' \
+ --dataset cub2002011 \
+ --base_architecture resnet34 \
+ --num_workers 4 \
+ --gpu_id 0 \
+ --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/protopnet/resnet34/2025-03-17_15-17-17/'
 
-# python src/protopnet/models_local_analysis.py \
-#  --data_dir '/nas-ctm01/datasets/public/cub2002011-dataset' \
-#  --dataset cub2002011 \
-#  --base_architecture vgg16 \
-#  --num_workers 4 \
-#  --gpu_id 0 \
-#  --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/protopnet/vgg16/2025-03-17_15-17-21/'
+python src/protopnet/models_local_analysis.py \
+ --data_dir '/nas-ctm01/datasets/public/cub2002011-dataset' \
+ --dataset cub2002011 \
+ --base_architecture vgg16 \
+ --num_workers 4 \
+ --gpu_id 0 \
+ --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/protopnet/vgg16/2025-03-17_15-17-21/'
 
 echo "CUB2002011 | Finished | Local Analysis"
