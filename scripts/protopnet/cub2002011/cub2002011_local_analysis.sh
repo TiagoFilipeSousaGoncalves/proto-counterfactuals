e2ef071b83892b778875fdfc390f312624c8a65d
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-04-01 17:54:51 +0100

    Updated: scripts/protopnet/cub2002011/cub2002011_local_analysis.sh

--- scripts/protopnet/cub2002011/cub2002011_local_analysis.sh
+++ scripts/protopnet/cub2002011/cub2002011_local_analysis.sh
@@ -9,7 +9,7 @@
 
 echo "CUB2002011 | Started | Local Analysis"
 echo "CUB2002011 | ProtoPNet"
-python code/protopnet/models_local_analysis.py \
+python src/protopnet/models_local_analysis.py \
  --data_dir '/nas-ctm01/datasets/public/cub2002011-dataset' \
  --dataset cub2002011 \
  --base_architecture densenet121 \
@@ -17,7 +17,7 @@ python code/protopnet/models_local_analysis.py \
  --gpu_id 0 \
  --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/protopnet/densenet121/2025-03-14_11-19-56/'
 
-# python code/protopnet/models_local_analysis.py \
+# python src/protopnet/models_local_analysis.py \
 #  --data_dir '/nas-ctm01/datasets/public/cub2002011-dataset' \
 #  --dataset cub2002011 \
 #  --base_architecture resnet34 \
@@ -25,7 +25,7 @@ python code/protopnet/models_local_analysis.py \
 #  --gpu_id 0 \
 #  --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/protopnet/resnet34/2025-03-17_15-17-17/'
 
-# python code/protopnet/models_local_analysis.py \
+# python src/protopnet/models_local_analysis.py \
 #  --data_dir '/nas-ctm01/datasets/public/cub2002011-dataset' \
 #  --dataset cub2002011 \
 #  --base_architecture vgg16 \
