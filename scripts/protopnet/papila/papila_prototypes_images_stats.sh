77780c8241d91c496d444b249695dda865a61536
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-04-04 19:01:12 +0100

    Updated: scripts/protopnet/papila/papila_prototypes_images_stats.sh

---
+++ scripts/protopnet/papila/papila_prototypes_images_stats.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -p cpu_8cores                     # Partition
+#SBATCH --qos=cpu_8cores                       # QOS
+#SBATCH --job-name=pla_pis                # Job name
+#SBATCH -o pla_pis.out                  # STDOUT
+#SBATCH -e pla_pis.err                  # STDERR
+
+
+
+echo "PAPILA | Started | Prototype Image Stats"
+python src/protopnet/prototypes_images_stats.py \
+ --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/papila/protopnet/densenet121/2025-03-23_09-59-10/'
+
+python src/protopnet/prototypes_images_stats.py \
+ --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/papila/protopnet/resnet34/2025-03-23_09-59-09/'
+
+python src/protopnet/prototypes_images_stats.py \
+ --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/papila/protopnet/vgg16/2025-03-23_15-12-24'
+
+echo "PAPILA | Finished | Prototype Image Stats"
