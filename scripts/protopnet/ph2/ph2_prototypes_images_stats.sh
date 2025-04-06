1f99a30f7cb1f32effae9900d49db88939d73b63
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-04-07 00:22:22 +0100

    Updated: scripts/protopnet/ph2/ph2_prototypes_images_stats.sh

--- scripts/protopnet/ph2/ph2_prototypes_images_stats.sh
+++ scripts/protopnet/ph2/ph2_prototypes_images_stats.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
+#SBATCH -p cpu_8cores                   # Partition
+#SBATCH --qos=cpu_8cores                # QOS
+#SBATCH --job-name=ph2_pis              # Job name
+#SBATCH -o ph2_pis.out                  # STDOUT
+#SBATCH -e ph2_pis.err                  # STDERR
 
 
 
 echo "Started | PH2 | Prototype Image Stats"
+echo "PH2 | ProtoPNet"
+python src/protopnet/prototypes_images_stats.py \
+ --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/ph2/protopnet/densenet121/2025-03-24_16-19-14/'
 
+python src/protopnet/prototypes_images_stats.py \
+ --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/ph2/protopnet/resnet34/2025-03-24_18-30-11/'
 
-model="dppnet"
-
-
-if [ $model == "ppnet" ]
-then
-    echo "PH2 | ProtoPNet"
-    python code/protopnet/prototypes_images_stats.py --checkpoint ph2/protopnet/densenet121/2022-12-06_15-51-53/
-    python code/protopnet/prototypes_images_stats.py --checkpoint ph2/protopnet/densenet161/2022-12-06_19-46-07/
-    python code/protopnet/prototypes_images_stats.py --checkpoint ph2/protopnet/resnet34/2022-12-06_22-45-55/
-    python code/protopnet/prototypes_images_stats.py --checkpoint ph2/protopnet/resnet152/2022-12-07_00-40-00/
-    python code/protopnet/prototypes_images_stats.py --checkpoint ph2/protopnet/vgg16/2022-12-07_00-42-57/
-    python code/protopnet/prototypes_images_stats.py --checkpoint ph2/protopnet/vgg19/2022-12-07_02-48-40/
-elif [ $model == 'dppnet' ]
-then
-    echo "PH2 | Deformable ProtoPNet"
-    python code/deformable-protopnet/prototypes_images_stats.py --checkpoint ph2/deformable-protopnet/densenet121/2023-01-02_08-43-56/
-    # python code/deformable-protopnet/prototypes_images_stats.py --checkpoint ph2/deformable-protopnet/densenet161/XXX/
-    python code/deformable-protopnet/prototypes_images_stats.py --checkpoint ph2/deformable-protopnet/resnet34/2023-01-02_10-08-37/
-    # python code/deformable-protopnet/prototypes_images_stats.py --checkpoint ph2/deformable-protopnet/resnet152/XXX/
-    python code/deformable-protopnet/prototypes_images_stats.py --checkpoint ph2/deformable-protopnet/vgg16/2023-01-04_10-43-58/
-    # python code/deformable-protopnet/prototypes_images_stats.py --checkpoint ph2/deformable-protopnet/vgg19/XXX/
-else
-    echo "Error"
-fi
+python src/protopnet/prototypes_images_stats.py \
+ --results_dir '/nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/ph2/protopnet/vgg16/2025-03-24_19-06-46/'
 
 echo "Finished | PH2 | Prototype Image Stats"
