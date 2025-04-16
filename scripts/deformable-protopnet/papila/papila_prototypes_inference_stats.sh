5993a2e3807c346bdc5a033840156d92746e601d
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-04-16 15:45:17 +0100

    Updated: scripts/deformable-protopnet/_/_prototypes_images_stats.sh-prototypes_inference_stats.sh

--- scripts/deformable-protopnet/papila/papila_prototypes_inference_stats.sh
+++ scripts/deformable-protopnet/papila/papila_prototypes_inference_stats.sh
@@ -10,11 +10,11 @@
 echo "CUB2002011 | Started | Prototype Inference Stats"
 echo "CUB2002011 | Deformable ProtoPNet"
 python src/deformable-protopnet/prototypes_inference_stats.py \
- --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/deformable-protopnet/densenet121/2025-03-24_09-39-04/
+ --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/papila/deformable-protopnet/densenet121/2025-03-26_11-31-33/
 
 python src/deformable-protopnet/prototypes_inference_stats.py \
- --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/deformable-protopnet/resnet34/2025-03-24_23-56-33/
+ --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/papila/deformable-protopnet/resnet34/2025-03-26_12-12-55/
 
 python src/deformable-protopnet/prototypes_inference_stats.py \
- --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/deformable-protopnet/vgg16/2025-03-24_23-59-23/
+ --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/papila/deformable-protopnet/vgg16/2025-03-26_11-31-32/
 echo "CUB2002011 | Finished | Prototype Inference Stats"
--- scripts/deformable-protopnet/ph2/ph2_prototypes_inference_stats.sh
+++ scripts/deformable-protopnet/ph2/ph2_prototypes_inference_stats.sh
@@ -10,11 +10,11 @@
 echo "CUB2002011 | Started | Prototype Inference Stats"
 echo "CUB2002011 | Deformable ProtoPNet"
 python src/deformable-protopnet/prototypes_inference_stats.py \
- --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/deformable-protopnet/densenet121/2025-03-24_09-39-04/
+ --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/ph2/deformable-protopnet/densenet121/2025-03-25_20-10-27/
 
 python src/deformable-protopnet/prototypes_inference_stats.py \
- --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/deformable-protopnet/resnet34/2025-03-24_23-56-33/
+ --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/ph2/deformable-protopnet/resnet34/2025-03-25_20-54-57/
 
 python src/deformable-protopnet/prototypes_inference_stats.py \
- --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/cub2002011/deformable-protopnet/vgg16/2025-03-24_23-59-23/
+ --results_dir /nas-ctm01/homes/tgoncalv/proto-counterfactuals/results/ph2/deformable-protopnet/vgg16/2025-03-25_18-54-27/
 echo "CUB2002011 | Finished | Prototype Inference Stats"
