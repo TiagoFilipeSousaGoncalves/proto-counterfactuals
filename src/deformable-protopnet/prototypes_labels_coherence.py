07ee595839a06e1611befca01e33d91593f22867
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-04-16 23:33:19 +0100

    Updated: scripts/deformable-protopnet/

--- src/deformable-protopnet/prototypes_labels_coherence.py
+++ src/deformable-protopnet/prototypes_labels_coherence.py
@@ -2,185 +2,177 @@
 import os
 import argparse
 import numpy as np
-import pandas as pd
+import pickle
 from itertools import combinations
 from scipy.stats import wasserstein_distance
 from statsmodels.stats.inter_rater import fleiss_kappa, aggregate_raters
 
 
 
-# Command Line Interface
-# Create the parser
-parser = argparse.ArgumentParser()
-
-# Add the arguments
-# Dataset
-parser.add_argument('--dataset', type=str, required=True, choices=["CUB2002011", "PAPILA", "PH2", "STANFORDCARS"], help="Data set: CUB2002011, PAPILA, PH2, STANFORDCARS.")
-
-# Checkpoint(s)
-parser.add_argument('--append-checkpoints', action="append", type=str, required=True, help="Path to the model checkpoint(s).")
-
-# Distance type
-parser.add_argument("--coherence_metric", type=str, required=True, choices=["fleiss_kappa", "earth_movers_distance"])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True, choices=["cub2002011", "papila", "ph2", "STANFORDCARS"], help="Data set: CUB2002011, PAPILA, PH2, STANFORDCARS.")
+    parser.add_argument('--result_dir_list', action="append", type=str, required=True, help="Paths to the models result directories.")
+    parser.add_argument("--coherence_metric", type=str, required=True, choices=["fleiss_kappa", "earth_movers_distance"])
+    args = parser.parse_args()
 
 
-# Parse the arguments
-args = parser.parse_args()
-
-
-# Get the dataset
-DATASET = args.dataset
-
-# Get the path of the CSV that contains the analysis
-CHECKPOINTS = args.append_checkpoints
-
-# Coherence metric
-COHERENCE_METRIC = args.coherence_metric
+    # Constants
+    DATASET = args.dataset
+    RESULT_DIR_LIST = args.result_dir_list
+    COHERENCE_METRIC = args.coherence_metric
 
 
 
-# Get the number of classes for the computation of the coherence metric
-if DATASET == "CUB2002011": 
-    N_CLASSES = 200
-elif DATASET == "PAPILA":
-    N_CLASSES = 3
-elif DATASET == "PH2":
-    N_CLASSES = 3
-elif DATASET == "STANFORDCARS":
-    pass
+    # Get the number of classes for the computation of the coherence metric
+    if DATASET == "cub2002011": 
+        N_CLASSES = 200
+    elif DATASET == "papila":
+        N_CLASSES = 3
+    elif DATASET == "ph2":
+        N_CLASSES = 3
+    elif DATASET == "STANFORDCARS":
+        pass
 
 
 
-# Create a list of multiple checkpoints' stats'
-proto_stats_df_list = list()
+    # Create a list of multiple checkpoints' stats'
+    proto_stats_list = list()
 
-# Populate the list
-for checkpoint in CHECKPOINTS:
-    
-    # Open the .CSV file
-    proto_stats_df = pd.read_csv(filepath_or_buffer=os.path.join("results", checkpoint, "analysis", "local", "analysis.csv"), sep=",", header=0)
-    # print(proto_stats_df.head())
-
-    # Get rows where ground-truth is equal to the predicted label
-    proto_stats_pr_df = proto_stats_df.copy()[["Image Filename", "Ground-Truth Label", "Predicted Label", "Number of Prototypes Connected to the Class Identity", "Top-10 Prototypes Class Identities"]][proto_stats_df["Ground-Truth Label"]==proto_stats_df["Predicted Label"]]
-    # print(proto_stats_pr_df.head())
-
-    # Reset index so that indices match the number of rows
-    proto_stats_pr_df = proto_stats_pr_df.reset_index()
-
-    # Append to the list
-    proto_stats_df_list.append(proto_stats_pr_df)
-
-
-
-# Create a dictionary to append this results
-label_coherence_dict = dict()
-coherence_values = list()
-
-
-
-# Go through the list of proto stats df
-for proto_stats_pr_df in proto_stats_df_list:
-
-    # Iterate through rows of the current df
-    for index, row in proto_stats_pr_df.iterrows():
-
-        # Get image filename and create an entrance in the label_coherence_dict (if not available)
-        if row["Image Filename"] not in label_coherence_dict.keys():
-            label_coherence_dict[row["Image Filename"]] = dict()
-            label_coherence_dict[row["Image Filename"]]["Ground-Truth Label"] = 0
-            label_coherence_dict[row["Image Filename"]]["Top-10 Prototypes Models"] = list()
-            label_coherence_dict[row["Image Filename"]]["Prototype Label Coherence"] = 0
-
-
-        # Get label and add it to our dictionary of results
-        label = row["Ground-Truth Label"]
-        label_coherence_dict[row["Image Filename"]]["Ground-Truth Label"] = label
-
-
-        # Get the cls identity of top-k most activated prototypes
-        top_k_proto = row["Top-10 Prototypes Class Identities"]
-
-        # Apply a processing to this string
-        top_k_proto = top_k_proto.split('[')[1]
-        top_k_proto = top_k_proto.split(']')[0]
-        top_k_proto = [int(i) for i in top_k_proto.split(',')]
-        # print(top_k_proto)
-
-        # Append this to teh dictionary of results
-        label_coherence_dict[row["Image Filename"]]["Top-10 Prototypes Models"].append(top_k_proto)
-
-
-
-# Iterate through the dictionary of results
-for image_filename in label_coherence_dict.keys():
-
-    # Compute only for the cases where we have all the n models
-    if len(label_coherence_dict[image_filename]["Top-10 Prototypes Models"]) == len(CHECKPOINTS):
+    # Populate the list
+    for results_dir in RESULT_DIR_LIST:
         
-        # Get the set of top-10 activated prototypes per image
-        prototypes_among_models = label_coherence_dict[image_filename]["Top-10 Prototypes Models"]
-        prototypes_among_models = np.array(prototypes_among_models)
+        # Open the .CSV file
+        proto_stats_pkl_fpath = os.path.join(results_dir, "analysis", "local", "analysis.pkl")
+        with open(proto_stats_pkl_fpath, "rb") as f:
+            proto_stats = pickle.load(f)
 
-        
-        # Using Fleiss Kappa
-        if COHERENCE_METRIC == "fleiss_kappa":
+        # Get cases where ground-truth is equal to the predicted label for a processed pickle file
+        proto_stats_pr = {
+            "Image Filename":list(),
+            "Ground-Truth Label":list(),
+            "Predicted Label":list(),
+            "Number of Prototypes Connected to the Class Identity":list(),
+            "Top-10 Prototypes Class Identities":list()
+        }
+
+        img_fnames = proto_stats["Image Filename"]
+        gt_labels = proto_stats["Ground-Truth Label"]
+        pred_labels = proto_stats["Predicted Label"]
+        nr_proto_cli = proto_stats["Number of Prototypes Connected to the Class Identity"]
+        topk_proto_cli = proto_stats["Top-10 Prototypes Class Identities"]
+
+        for i in range(len(img_fnames)):
+            if int(gt_labels[i]) == int(pred_labels[i]):
+                proto_stats_pr["Image Filename"].append(img_fnames[i])
+                proto_stats_pr["Ground-Truth Label"].append(gt_labels[i])
+                proto_stats_pr["Predicted Label"].append(pred_labels[i])
+                proto_stats_pr["Number of Prototypes Connected to the Class Identity"].append(nr_proto_cli[i])
+                proto_stats_pr["Top-10 Prototypes Class Identities"].append(topk_proto_cli[i])
+
+        # Append to the list
+        proto_stats_list.append(proto_stats_pr)
+
+
+
+    # Create a dictionary to append this results
+    label_coherence_dict = dict()
+    coherence_values = list()
+
+
+
+    # Go through the list of proto stats df
+    for proto_stats_pr_ in proto_stats_list:
+
+        # Iterate through rows of the current df
+        for j in range(len(proto_stats_pr_["Image Filename"])):
+
+            # Get image filename and create an entrance in the label_coherence_dict (if not available)
+            if proto_stats_pr_["Image Filename"][j] not in label_coherence_dict.keys():
+                label_coherence_dict[proto_stats_pr_["Image Filename"][j]] = dict()
+                label_coherence_dict[proto_stats_pr_["Image Filename"][j]]["Ground-Truth Label"] = 0
+                label_coherence_dict[proto_stats_pr_["Image Filename"][j]]["Top-10 Prototypes Models"] = list()
+                label_coherence_dict[proto_stats_pr_["Image Filename"][j]]["Prototype Label Coherence"] = 0
+
+
+            # Get label and add it to our dictionary of results
+            label = proto_stats_pr_["Ground-Truth Label"][j]
+            label_coherence_dict[proto_stats_pr_["Image Filename"][j]]["Ground-Truth Label"] = label
+
+
+            # Get the cls identity of top-k most activated prototypes
+            top_k_proto = proto_stats_pr_["Top-10 Prototypes Class Identities"][j]
+
+
+            # Append this to the dictionary of results
+            label_coherence_dict[proto_stats_pr_["Image Filename"][j]]["Top-10 Prototypes Models"].append(top_k_proto)
+
+
+
+    # Iterate through the dictionary of results
+    for image_filename in label_coherence_dict.keys():
+
+        # Compute only for the cases where we have all the n models
+        if len(label_coherence_dict[image_filename]["Top-10 Prototypes Models"]) == len(RESULT_DIR_LIST):
             
-            # Transpose the vector so we have the right format to compute the Fleiss Kappa
-            prototypes_among_models = np.transpose(prototypes_among_models)
+            # Get the set of top-10 activated prototypes per image
+            prototypes_among_models = label_coherence_dict[image_filename]["Top-10 Prototypes Models"]
+            prototypes_among_models = np.array(prototypes_among_models)
 
-            # Compute the Fleiss Kappa
-            fleiss_kappa_arr, categories_arr = aggregate_raters(data=prototypes_among_models, n_cat=N_CLASSES)
-            fleiss_kappa_value = fleiss_kappa(table=fleiss_kappa_arr, method='uniform')
-            coherence_res = fleiss_kappa_value
-        
-
-        # Using Earth Movers Distance
-        elif COHERENCE_METRIC == "earth_movers_distance":
-
-            # Get the combinations
-            idx_comb = combinations(range(len(prototypes_among_models)), 2)
-            idx_comb = list(idx_comb)
             
-            # Iterate through these combinations
-            wass_distances = list()
-            for comb in idx_comb:
-                wd = wasserstein_distance(prototypes_among_models[comb[0]], prototypes_among_models[comb[1]])
-                wass_distances.append(wd)
+            # Using Fleiss Kappa
+            if COHERENCE_METRIC == "fleiss_kappa":
+                
+                # Transpose the vector so we have the right format to compute the Fleiss Kappa
+                prototypes_among_models = np.transpose(prototypes_among_models)
+
+                # Compute the Fleiss Kappa
+                fleiss_kappa_arr, categories_arr = aggregate_raters(data=prototypes_among_models, n_cat=N_CLASSES)
+                fleiss_kappa_value = fleiss_kappa(table=fleiss_kappa_arr, method='uniform')
+                coherence_res = fleiss_kappa_value
             
-            # Compute coherence as the mean of these distances
-            coherence_res = np.mean(wass_distances)
+
+            # Using Earth Movers Distance
+            elif COHERENCE_METRIC == "earth_movers_distance":
+
+                # Get the combinations
+                idx_comb = combinations(range(len(prototypes_among_models)), 2)
+                idx_comb = list(idx_comb)
+                
+                # Iterate through these combinations
+                wass_distances = list()
+                for comb in idx_comb:
+                    wd = wasserstein_distance(prototypes_among_models[comb[0]], prototypes_among_models[comb[1]])
+                    wass_distances.append(wd)
+                
+                # Compute coherence as the mean of these distances
+                coherence_res = np.mean(wass_distances)
 
 
 
-        # Add this to the dictionary
-        label_coherence_dict[image_filename]["Prototype Label Coherence"] = coherence_res
+            # Add this to the dictionary
+            label_coherence_dict[image_filename]["Prototype Label Coherence"] = coherence_res
 
-        # Add this to the list of values
-        coherence_values.append(coherence_res)
-
-
-
-# Open a file to save a small report w/ .TXT extension
-if os.path.exists(os.path.join("results", DATASET.lower(), "deformable-protopnet", "prototype_label_coherence.txt")):
-    os.remove(os.path.join("results", DATASET.lower(), "deformable-protopnet", "prototype_label_coherence.txt"))
-
-report = open(os.path.join("results", DATASET.lower(), "deformable-protopnet", "prototype_label_coherence.txt"), "at")
+            # Add this to the list of values
+            coherence_values.append(coherence_res)
 
 
 
-# Get the mean value of coherence
-mean_value = np.mean(coherence_values)
-
-# Add this value to a report
-if COHERENCE_METRIC == "fleiss_kappa":
-    report.write(f"Coherence (Fleiss Kappa): {mean_value}\n")
-elif COHERENCE_METRIC == "earth_movers_distance":
-    report.write(f"Coherence (Earth Movers Distance)): {mean_value}\n")
-
-# Close report
-report.close()
+    # Open a file to save a small report w/ .TXT extension
+    if os.path.exists(os.path.join("results", DATASET.lower(), "deformable-protopnet", "prototype_label_coherence.txt")):
+        os.remove(os.path.join("results", DATASET.lower(), "deformable-protopnet", "prototype_label_coherence.txt"))
+    report = open(os.path.join("results", DATASET.lower(), "deformable-protopnet", "prototype_label_coherence.txt"), "at")
 
 
 
-print("Finished.")
+    # Get the mean value of coherence
+    mean_value = np.mean(coherence_values)
+
+    # Add this value to a report
+    if COHERENCE_METRIC == "fleiss_kappa":
+        report.write(f"Coherence (Fleiss Kappa): {mean_value}\n")
+    elif COHERENCE_METRIC == "earth_movers_distance":
+        report.write(f"Coherence (Earth Movers Distance)): {mean_value}\n")
+
+    # Close report
+    report.close()
