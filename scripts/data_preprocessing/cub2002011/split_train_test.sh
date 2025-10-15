aed8c3850115628f188f6572142165b6449f1efb
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-10-15 16:24:46 +0100

    Updated: scripts/data_preprocessing/cub2002011/split_train_test.sh, src/data_preprocessing/cub2002011/split_train_test.py

--- scripts/data_preprocessing/cub2002011/split_train_test.sh
+++ scripts/data_preprocessing/cub2002011/split_train_test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH -p cpu_8cores                     # Partition
-#SBATCH --qos=cpu_8cores                       # QOS
+#SBATCH --partition=fct                 
+#SBATCH --qos=gpu082112025    
+#SBATCH --mem=12288M                   
 #SBATCH --job-name=cub-p-tts                 # Job name
 #SBATCH -o cub2002011_tts.out                  # STDOUT
 #SBATCH -e cub2002011_tts.err                  # STDERR
 
 echo 'Started split train-val-test of CUB2002011.'
 
-python src/data_preprocessing/cub2002011/split_train_test.py --data_dir '/nas-ctm01/datasets/public/cub2002011-dataset'
+python src/data_preprocessing/cub2002011/split_train_test.py --data_dir '/users5/cpca082112025/tgoncalves/proto-counterfactuals/data/cub2002011-dataset'
 
 echo 'Finished split train-val-test of CUB2002011.'
--- src/data_preprocessing/cub2002011/split_train_test.py
+++ src/data_preprocessing/cub2002011/split_train_test.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 
 # Sklearn Imports
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 
@@ -20,27 +20,23 @@ if __name__ == "__main__":
     parser.add_argument('--train_size', type=float, default=0.70, help="Set the train size (%).")
     parser.add_argument('--val_size', type=float, default=0.10, help="Set the validation size (%)")
     parser.add_argument('--test_size', type=float, default=0.20, help="Set the test size (%).")
+    parser.add_argument('--n_folds', type=int, default=5, help="Set the number of folds for cross-validation.")
     args = parser.parse_args()
 
 
     # Directories and Files
     data_dir = args.data_dir
 
-    # Create new directories
-    for directory in ["train", "val", "test"]:
-        if not os.path.isdir(os.path.join(data_dir, "processed", directory, "cropped")):
-            os.makedirs(os.path.join(data_dir, "processed", directory, "cropped"))
+    # TODO: Remove this uppon testing
+    # for directory in ["train", "val", "test"]:
+    #     if not os.path.isdir(os.path.join(data_dir, "processed", directory, "cropped")):
+    #         os.makedirs(os.path.join(data_dir, "processed", directory, "cropped"))
 
 
 
     # Open images.txt
     images = np.genfromtxt(os.path.join(data_dir, "CUB_200_2011", "images.txt"), dtype=str)
     # print(images)
-
-    # Note: We will build a train-test split with validation, so, we will do it from scratch
-    # Open train_test_split.txt
-    # train_test_split = np.genfromtxt(os.path.join(data_dir, "CUB_200_2011", "train_test_split.txt"), dtype=str)
-    # print(train_test_split)
 
     # Create a new list with the ID's and classes
     images_ids = list()
@@ -53,61 +49,113 @@ if __name__ == "__main__":
         images_ids.append(img_id)
         image_fnames.append(img_fname)
         images_classes.append(img_cl)
-    # print(images_ids)
-    # print(images_classes)
-    # print(len(images_ids), len(images_classes), np.unique(images_classes))
 
+
+
+    # Apply 5-fold cross validation split strategy
     assert (args.train_size + args.val_size + args.test_size) == 1.0
-    X_train_val, X_test, y_train_val, y_test = train_test_split(image_fnames, images_classes, train_size=(args.train_size + args.val_size), random_state=args.seed, stratify=images_classes)
-    X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, train_size=(args.train_size/(args.train_size + args.val_size)), random_state=args.seed, stratify=y_train_val)
-    assert round(len(X_train)/len(image_fnames), 2) == args.train_size
-    assert round(len(X_val)/len(image_fnames), 2) == args.val_size
-    assert round(len(X_test)/len(image_fnames), 2) == args.test_size
 
-    # Save this into .CSVs
-    X_train_df = pd.DataFrame.from_dict({'train':list(X_train)})
-    X_train_df.to_csv(os.path.join(data_dir, "processed", "train.csv"), index=False)
+    # Create dictionaries to save the splits
+    trainval_dict = dict()
+    test_dict = dict()
+    train_dict = dict()
+    val_dict = dict()
 
-    X_val_df = pd.DataFrame.from_dict({'val':list(X_val)})
-    X_val_df.to_csv(os.path.join(data_dir, "processed", "val.csv"), index=False)
+    sss_trainval_test = StratifiedShuffleSplit(
+        n_splits=args.n_folds,
+        train_size=args.train_size + args.val_size,
+        random_state=args.seed
+    )
+    sss_train_val = StratifiedShuffleSplit(
+        n_splits=1,
+        train_size=(args.train_size / (args.train_size + args.val_size)),
+        random_state=args.seed
+    )
 
-    X_test_df = pd.DataFrame.from_dict({'test':list(X_test)})
-    X_test_df.to_csv(os.path.join(data_dir, "processed", "test.csv"), index=False)
+    X = image_fnames
+    y = images_classes
+    for fold, (train_index, test_index) in enumerate(sss_trainval_test.split(X, y)):
+        trainval_dict[fold] = {
+            'image_fnames': [X[i] for i in train_index],
+            'images_classes': [y[i] for i in train_index]
+        }
+        test_dict[fold] = {
+            'image_fnames': [X[i] for i in test_index],
+            'images_classes': [y[i] for i in test_index]
+        }
+    for fold in range(args.n_folds):
+        X = trainval_dict[fold]['image_fnames']
+        y = trainval_dict[fold]['images_classes']
+        for _, (train_index, test_index) in enumerate(sss_train_val.split(X, y)):
+            train_dict[fold] = {
+                'image_fnames': [X[i] for i in train_index],
+                'images_classes': [y[i] for i in train_index]
+            }
+            val_dict[fold] = {
+                'image_fnames': [X[i] for i in test_index],
+                'images_classes': [y[i] for i in test_index]
+            }
+
+    # TODO: Remove after testing
+    # X_train_val, X_test, y_train_val, y_test = train_test_split(image_fnames, images_classes, train_size=(args.train_size + args.val_size), random_state=args.seed, stratify=images_classes)
+    # X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, train_size=(args.train_size/(args.train_size + args.val_size)), random_state=args.seed, stratify=y_train_val)
+    # assert round(len(X_train)/len(image_fnames), 2) == args.train_size
+    # assert round(len(X_val)/len(image_fnames), 2) == args.val_size
+    # assert round(len(X_test)/len(image_fnames), 2) == args.test_size
 
 
-    # Let's split the data
-    for split_idx, data_split in enumerate([X_train, X_val, X_test]):
-        for image_fname in tqdm.tqdm(data_split):
-            # print(image_fname)
+    # Go through each fold
+    for fold in range(args.n_folds):
+        assert len(image_fnames) == len(train_dict[fold]['image_fnames']) + len(val_dict[fold]['image_fnames']) + len(test_dict[fold]['image_fnames'])
 
-            # Check if image is for train or test
-            if split_idx == 0:
-                split_dir = "train"
-            elif split_idx == 1:
-                split_dir = "val"
-            else:
-                split_dir = "test"
-            
+        # Create fold directory
+        os.makedirs(os.path.join(data_dir, "processed", f"kf_{fold}"), exist_ok=True)
 
-            # Create a folder for the complete images
-            if not os.path.isdir(os.path.join(data_dir, "processed", split_dir, "images")):
-                # print(os.path.join(data_dir, "processed", split_dir, "images"))
-                os.makedirs(os.path.join(data_dir, "processed", split_dir, "images"))
-            
+        # Get the data splits
+        X_train = train_dict[fold]['image_fnames']
+        X_val = val_dict[fold]['image_fnames']
+        X_test = test_dict[fold]['image_fnames']
 
-            # Get image class folder
-            img_class_folder = image_fname.split("/")[0]
-            # print(img_class_folder)
+        # Save this into .CSVs
+        X_train_df = pd.DataFrame.from_dict({'train':list(X_train)})
+        X_train_df.to_csv(os.path.join(data_dir, "processed", f"kf_{fold}", "train.csv"), index=False)
 
-            # Create this folder (if it does not exist)
-            if not os.path.isdir(os.path.join(data_dir, "processed", split_dir, "images", img_class_folder)):
-                # print(os.path.join(data_dir, "processed", split_dir, "images", img_class_folder))
-                os.makedirs(os.path.join(data_dir, "processed", split_dir, "images", img_class_folder))
+        X_val_df = pd.DataFrame.from_dict({'val':list(X_val)})
+        X_val_df.to_csv(os.path.join(data_dir, "processed", f"kf_{fold}", "val.csv"), index=False)
 
-            
-            # Copy this image to this split folder
-            src = os.path.join(data_dir, "CUB_200_2011", "images", image_fname)
-            dst = os.path.join(data_dir, "processed", split_dir, "images", image_fname)
-            # print(src)
-            # print(dst)
-            shutil.copy(src, dst)
+        X_test_df = pd.DataFrame.from_dict({'test':list(X_test)})
+        X_test_df.to_csv(os.path.join(data_dir, "processed", f"kf_{fold}", "test.csv"), index=False)
+
+
+        # Let's split the data
+        for split_idx, data_split in enumerate([X_train, X_val, X_test]):
+            for image_fname in tqdm.tqdm(data_split):
+                # print(image_fname)
+
+                # Check if image is for train or test
+                if split_idx == 0:
+                    split_dir = "train"
+                elif split_idx == 1:
+                    split_dir = "val"
+                else:
+                    split_dir = "test"
+                
+
+                # Create a folder for the complete images
+                os.makedirs(os.path.join(data_dir, "processed", f"kf_{fold}", split_dir, "images"), exist_ok=True)
+                
+
+                # Get image class folder
+                img_class_folder = image_fname.split("/")[0]
+                # print(img_class_folder)
+
+                # Create this folder (if it does not exist)
+                os.makedirs(os.path.join(data_dir, "processed", f"kf_{fold}", split_dir, "images", img_class_folder), exist_ok=True)
+
+                
+                # Copy this image to this split folder
+                src = os.path.join(data_dir, "CUB_200_2011", "images", image_fname)
+                dst = os.path.join(data_dir, "processed", f"kf_{fold}", split_dir, "images", image_fname)
+                # print(src)
+                # print(dst)
+                shutil.copy(src, dst)
