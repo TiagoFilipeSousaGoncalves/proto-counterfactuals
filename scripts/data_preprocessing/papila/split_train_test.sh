267e64df80e793201c932aaaa5c1fcd92e807c7c
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-12-01 12:36:41 +0000

    Updated: scripts/data_preprocessing/papila/split_train_test.sh

--- scripts/data_preprocessing/papila/split_train_test.sh
+++ scripts/data_preprocessing/papila/split_train_test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH -p cpu_8cores                     # Partition
-#SBATCH --qos=cpu_8cores                       # QOS
-#SBATCH --job-name=pla-p-tts                 # Job name
+#SBATCH --partition=fct                 
+#SBATCH --qos=gpu082112025    
+#SBATCH --mem=12288M  
 #SBATCH -o papila_tts.out                  # STDOUT
 #SBATCH -e papila_tts.err                  # STDERR
 
 echo 'Started split train-val-test of PAPILA.'
 
-python src/data_preprocessing/papila/split_train_test.py --data_dir '/nas-ctm01/datasets/public/MEDICAL/papila-dataset-glaucoma-fundus-images'
+python src/data_preprocessing/papila/split_train_test.py \
+ --data_dir '/users5/cpca082112025/shared/datasets/papila-dataset-glaucoma-fundus-images'
 
 echo 'Finished split train-val-test of PAPILA.'
