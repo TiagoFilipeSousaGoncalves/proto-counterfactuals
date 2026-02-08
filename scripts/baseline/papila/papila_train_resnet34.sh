b6f7c011118ebb1c90cad431602a2357189cfc82
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2026-02-08 20:35:52 +0000

    Updated: scripts/baseline/papila/papila_train_resnet34.sh, scripts/baseline/papila/papila_train_vgg16.sh

--- scripts/baseline/papila/papila_train_resnet34.sh
+++ scripts/baseline/papila/papila_train_resnet34.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH -p gpu_min32gb                     # Partition
-#SBATCH --qos=gpu_min32gb                       # QOS
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu082112025
+#SBATCH --mem=12288M
 #SBATCH --job-name=pla_r34                 # Job name
 #SBATCH -o pla_r34.out                  # STDOUT
 #SBATCH -e pla_r34.err                  # STDERR
@@ -11,11 +12,13 @@ echo "PAPILA | Started | Training"
 
 echo "Baseline | ResNet34"
 python src/baseline/models_train.py \
- --data_dir '/nas-ctm01/datasets/public/MEDICAL/papila-dataset-glaucoma-fundus-images' \
+ --data_dir '/users5/cpca082112025/shared/datasets/papila-dataset-glaucoma-fundus-images' \
  --dataset papila \
  --base_architecture resnet34 \
- --batchsize 32 \
- --num_workers 4 \
- --gpu_id 0
+ --batchsize 64 \
+ --num_workers 0 \
+ --gpu_id 0 \
+ --folds 0 1 2 3 4 \
+ --output_dir '/users5/cpca082112025/shared/experiments/tgoncalves/proto-counterfactuals/results'
 
 echo "PAPILA | Finished | Training"
--- scripts/baseline/papila/papila_train_vgg16.sh
+++ scripts/baseline/papila/papila_train_vgg16.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH -p gpu_min32gb                     # Partition
-#SBATCH --qos=gpu_min32gb                       # QOS
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu082112025
+#SBATCH --mem=12288M
 #SBATCH --job-name=pla_v16                 # Job name
 #SBATCH -o pla_v16.out                  # STDOUT
 #SBATCH -e pla_v16.err                  # STDERR
@@ -11,11 +12,13 @@ echo "PAPILA | Started | Training"
 
 echo "Baseline | VGG16"
 python src/baseline/models_train.py \
- --data_dir '/nas-ctm01/datasets/public/MEDICAL/papila-dataset-glaucoma-fundus-images' \
+ --data_dir '/users5/cpca082112025/shared/datasets/papila-dataset-glaucoma-fundus-images' \
  --dataset papila \
  --base_architecture vgg16 \
- --batchsize 32 \
- --num_workers 4 \
- --gpu_id 0
+ --batchsize 64 \
+ --num_workers 0 \
+ --gpu_id 0 \
+ --folds 0 1 2 3 4 \
+ --output_dir '/users5/cpca082112025/shared/experiments/tgoncalves/proto-counterfactuals/results'
 
 echo "PAPILA | Finished | Training"
