c08794a92b2e42e51587c8bd3346ec124a834588
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2026-02-08 20:28:48 +0000

    Updated: scripts/baseline/ph2/ph2_train_vgg16.sh

--- scripts/baseline/ph2/ph2_train_vgg16.sh
+++ scripts/baseline/ph2/ph2_train_vgg16.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#SBATCH -p gpu_min32gb                     # Partition
-#SBATCH --qos=gpu_min32gb                       # QOS
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu082112025
+#SBATCH --mem=12288M
 #SBATCH --job-name=ph2_v16                 # Job name
 #SBATCH -o ph2_v16.out                  # STDOUT
 #SBATCH -e ph2_v16.err                  # STDERR
@@ -11,11 +12,13 @@ echo "PH2 | Started | Training"
 
 echo "Baseline | VGG16"
 python src/baseline/models_train.py \
- --data_dir '/nas-ctm01/datasets/public/MEDICAL/ph2-database' \
+ --data_dir '/users5/cpca082112025/shared/datasets/ph2-database' \
  --dataset ph2 \
  --base_architecture vgg16 \
- --batchsize 32 \
- --num_workers 4 \
- --gpu_id 0
+ --batchsize 64 \
+ --num_workers 0 \
+ --gpu_id 0 \
+ --folds 0 1 2 3 4 \
+ --output_dir '/users5/cpca082112025/shared/experiments/tgoncalves/proto-counterfactuals/results'
 
 echo "PH2 | Finished | Training"
