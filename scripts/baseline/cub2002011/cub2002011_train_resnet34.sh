91744778b20a2ddae4099d873ebb0e7ffd7076ea
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2026-02-22 16:31:24 +0000

    Updated: scripts/baseline/cub2002011/cub2002011_train_resnet34.sh

--- scripts/baseline/cub2002011/cub2002011_train_resnet34.sh
+++ scripts/baseline/cub2002011/cub2002011_train_resnet34.sh
@@ -18,7 +18,7 @@ python src/baseline/models_train.py \
  --batchsize 64 \
  --num_workers 0 \
  --gpu_id 0 \
- --folds 1 2 3 4 \
+ --folds 4 \
  --timestamp 2026-02-11_10-51-44 \
  --output_dir '/users5/cpca082112025/shared/experiments/tgoncalves/proto-counterfactuals/results'
 
