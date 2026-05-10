051a26774fad83e28833c09f3e9640737ab444bb
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2026-05-10 19:15:00 +0100

    Updated: scripts/deformable-protopnet/papila/

--- scripts/deformable-protopnet/papila/papila_train_densenet121.sh
+++ scripts/deformable-protopnet/papila/papila_train_densenet121.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-#
-#SBATCH -p gpu_min32gb                     # Partition
-#SBATCH --qos=gpu_min32gb                       # QOS
-#SBATCH --job-name=pla_d121                 # Job name
-#SBATCH -o pla_d121.out           # STDOUT
-#SBATCH -e pla_d121.err           # STDERR
-
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu082112025
+#SBATCH --mem=12288M
+#SBATCH --job-name=papila_d121                # Job name
+#SBATCH -o papila_d121.out                  # STDOUT
+#SBATCH -e papila_d121.err                  # STDERR
 
 
 echo "Started | PAPILA | Training"
 echo "PAPILA | Deformable-ProtoPNet DenseNet121"
-
 python src/deformable-protopnet/models_train.py \
- --data_dir '/nas-ctm01/datasets/public/MEDICAL/papila-dataset-glaucoma-fundus-images' \
+ --data_dir '/users5/cpca082112025/shared/datasets/papila-dataset-glaucoma-fundus-images' \
  --dataset papila \
  --base_architecture densenet121 \
  --batchsize 32 \
  --subtractive_margin \
  --using_deform \
- --last_layer_fixed \
- --num_workers 4 \
- --gpu_id 0
+ --num_workers 0 \
+ --gpu_id 0 \
+ --folds 0 1 2 3 4 \
+ --output_dir '/users5/cpca082112025/shared/experiments/tgoncalves/proto-counterfactuals/results'
+ # --last_layer_fixed \
+ # --timestamp ''
 
-echo "PAPILA | FINISHED"
+echo "PAPILA | Finished | Training"
--- scripts/deformable-protopnet/papila/papila_train_resnet34.sh
+++ scripts/deformable-protopnet/papila/papila_train_resnet34.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-#
-#SBATCH -p gpu_min11gb                 # Partition
-#SBATCH --qos=gpu_min11gb                   # QOS
-#SBATCH --job-name=pla_r34                  # Job name
-#SBATCH -o pla_r34.out                  # STDOUT
-#SBATCH -e pla_r34.err                  # STDERR
-
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu082112025
+#SBATCH --mem=12288M
+#SBATCH --job-name=papila_r34                # Job name
+#SBATCH -o papila_r34.out                  # STDOUT
+#SBATCH -e papila_r34.err                  # STDERR
 
 
 echo "Started | PAPILA | Training"
 echo "PAPILA | Deformable-ProtoPNet ResNet34"
-
 python src/deformable-protopnet/models_train.py \
- --data_dir '/nas-ctm01/datasets/public/MEDICAL/papila-dataset-glaucoma-fundus-images' \
+ --data_dir '/users5/cpca082112025/shared/datasets/papila-dataset-glaucoma-fundus-images' \
  --dataset papila \
  --base_architecture resnet34 \
- --batchsize 16 \
+ --batchsize 32 \
  --subtractive_margin \
  --using_deform \
- --last_layer_fixed \
- --num_workers 4 \
- --gpu_id 0
+ --num_workers 0 \
+ --gpu_id 0 \
+ --folds 0 1 2 3 4 \
+ --output_dir '/users5/cpca082112025/shared/experiments/tgoncalves/proto-counterfactuals/results'
+ # --last_layer_fixed \
+ # --timestamp ''
 
-echo "Finished | PAPILA | Training"
+echo "PAPILA | Finished | Training"
